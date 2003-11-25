336932f91a494d59f963edf9c0fc2094bdee5be1
Rafal Krzewski <rafal.krzewski@caltha.pl>
2003-11-25 13:08:43 +0000

    added servlet file system provider

---
+++ ledge-web/src/main/java/org/objectledge/filesystem/impl/ServletFileProvider.java
@@ -0,0 +1,47 @@
+package org.objectledge.filesystem.impl;
+
+import java.io.InputStream;
+
+import javax.servlet.ServletContext;
+
+/**
+ * An implementation of FileService provider that operates on the ServletContext
+ * 
+ * <p>This is a read-only implementation. It is able to use WEB-INF/files for
+ * listing functionality. </p>
+ * 
+ * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
+ * @version $Id: ServletFileProvider.java,v 1.1 2003-11-25 13:08:43 fil Exp $
+ */
+public class ServletFileProvider 
+	extends ReadOnlyFileSystemProvider
+{
+	// instance variables /////////////////////////////////////////////////////////////////////////
+
+    /** the servlet context used for reading resources. */	
+	private ServletContext context;
+
+    // initialization /////////////////////////////////////////////////////////////////////////////
+    
+    /**
+     * Crates a new instance of the provider.
+     * 
+     * @param name the name of the provider.
+     * @param context the servlet context to read resources from.
+     */
+    public ServletFileProvider(String name, ServletContext context)
+    {
+        super(name);
+        this.context = context; 
+    }
+    
+    // public interface ///////////////////////////////////////////////////////////////////////////
+    
+    /**
+     * {@inheritDoc}
+     */
+    public InputStream getInputStream(String path) 
+    {
+		return context.getResourceAsStream(path);
+    }
+}
