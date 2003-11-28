5b93ce4b1b97fbaf2b43fe35f548f416c9aac81a
Rafal Krzewski <rafal.krzewski@caltha.pl>
2003-11-28 15:53:07 +0000

    make the component Type3 configurable

--- ledge-container/src/test/java/org/objectledge/test/FooComponent.java
+++ ledge-container/src/test/java/org/objectledge/test/FooComponent.java
@@ -5,15 +5,25 @@
  */
 package org.objectledge.test;
 
+import org.jcontainer.dna.Configuration;
+
 /**
  * A simple test component.
  *
  * @author <a href="Rafal.Krzewski">rafal@caltha.pl</a>
- * @version $Id: FooComponent.java,v 1.1 2003-11-28 11:15:39 fil Exp $
+ * @version $Id: FooComponent.java,v 1.2 2003-11-28 15:53:07 fil Exp $
  */
 public class FooComponent
 {
-    public FooComponent()
+    private Configuration configuration;
+    
+    public FooComponent(Configuration configuration)
     {
+        this.configuration = configuration;
+    }
+    
+    public Configuration getConfiguration()
+    {
+        return configuration;
     }
 }
