3b4683a031e25f3c622870c7cc0f6a661280619e
Paweł Potempski <pawel.potempski@caltha.pl>
2003-12-02 14:57:03 +0000

    empty test added

---
+++ ledge-web/src/test/java/org/objectledge/filesystem/impl/ServletFileProviderTest.java
@@ -0,0 +1,38 @@
+package org.objectledge.filesystem.impl;
+
+import junit.framework.TestCase;
+
+/**
+ * @author <a href="mailto:pablo@caltha.pl">Pawel Potempski</a>
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class ServletFileProviderTest extends TestCase
+{
+
+    /**
+     * Constructor for ServletFileProviderTest.
+     * 
+     * @param arg0 the file name.
+     */
+    public ServletFileProviderTest(String arg0)
+    {
+        super(arg0);
+    }
+
+	/**
+ 	*  
+ 	*/
+    public void testGetInputStream()
+    {
+    }
+
+	/**
+	 *
+	 */
+    public void testServletFileProvider()
+    {
+    }
+
+}
