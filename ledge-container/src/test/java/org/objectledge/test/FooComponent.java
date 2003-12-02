8cce4de14b51e1fe9ff8276dd0600f6bd55e61e7
Rafal Krzewski <rafal.krzewski@caltha.pl>
2003-12-02 15:24:22 +0000

    use logging

--- ledge-container/src/test/java/org/objectledge/test/FooComponent.java
+++ ledge-container/src/test/java/org/objectledge/test/FooComponent.java
@@ -6,24 +6,33 @@
 package org.objectledge.test;
 
 import org.jcontainer.dna.Configuration;
+import org.jcontainer.dna.Logger;
 
 /**
  * A simple test component.
  *
  * @author <a href="Rafal.Krzewski">rafal@caltha.pl</a>
- * @version $Id: FooComponent.java,v 1.2 2003-11-28 15:53:07 fil Exp $
+ * @version $Id: FooComponent.java,v 1.3 2003-12-02 15:24:22 fil Exp $
  */
 public class FooComponent
 {
     private Configuration configuration;
     
-    public FooComponent(Configuration configuration)
+    private Logger logger;
+    
+    public FooComponent(Configuration configuration, Logger logger)
     {
         this.configuration = configuration;
+        this.logger = logger;
     }
     
     public Configuration getConfiguration()
     {
         return configuration;
+    }
+    
+    public void log()
+    {
+        logger.info("yipee!");
     }
 }
