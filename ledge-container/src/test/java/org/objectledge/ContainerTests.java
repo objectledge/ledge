2415f4b2162dc175e466d1366029fa40851bff68
Michał Mach <michal.mach@caltha.pl>
2003-12-03 15:24:48 +0000

    license disclaimer added

--- ledge-container/src/test/java/org/objectledge/ContainerTests.java
+++ ledge-container/src/test/java/org/objectledge/ContainerTests.java
@@ -1,8 +1,31 @@
-/*
- * Copyright (c) 2003 Caltha Sp.J., All rights reserved
- * 
- * Created on Nov 28, 2003
- */
+// 
+// Copyright (c) 2003, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+// All rights reserved. 
+// 
+// Redistribution and use in source and binary forms, with or without modification,  
+// are permitted provided that the following conditions are met: 
+// 
+// * Redistributions of source code must retain the above copyright notice,  
+//       this list of conditions and the following disclaimer. 
+// * Redistributions in binary form must reproduce the above copyright notice,  
+//       this list of conditions and the following disclaimer in the documentation  
+//       and/or other materials provided with the distribution. 
+// * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
+//       nor the names of its contributors may be used to endorse or promote products  
+//       derived from this software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+// POSSIBILITY OF SUCH DAMAGE. 
+// 
+
 package org.objectledge;
 
 import junit.framework.Test;
@@ -12,7 +35,7 @@ import junit.framework.TestSuite;
  *
  *
  * @author <a href="Rafal.Krzewski">rafal@caltha.pl</a>
- * @version $Id: ContainerTests.java,v 1.2 2003-11-28 15:53:25 fil Exp $
+ * @version $Id: ContainerTests.java,v 1.3 2003-12-03 15:19:16 mover Exp $
  */
 public class ContainerTests
 {
