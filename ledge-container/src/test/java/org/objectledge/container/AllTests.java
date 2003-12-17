f4cb49d654685949876ef8e82cec250a889afdcb
Rafal Krzewski <rafal.krzewski@caltha.pl>
2003-12-17 10:04:18 +0000

    added LedgeContainer tests

--- ledge-container/src/test/java/org/objectledge/container/AllTests.java
+++ ledge-container/src/test/java/org/objectledge/container/AllTests.java
@@ -1,18 +1,18 @@
 // 
 // Copyright (c) 2003, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
 // All rights reserved. 
-// 
+//   
 // Redistribution and use in source and binary forms, with or without modification,  
 // are permitted provided that the following conditions are met: 
-// 
+//   
 // * Redistributions of source code must retain the above copyright notice,  
-//       this list of conditions and the following disclaimer. 
+//   this list of conditions and the following disclaimer. 
 // * Redistributions in binary form must reproduce the above copyright notice,  
-//       this list of conditions and the following disclaimer in the documentation  
-//       and/or other materials provided with the distribution. 
+//   this list of conditions and the following disclaimer in the documentation  
+//   and/or other materials provided with the distribution. 
 // * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
-//       nor the names of its contributors may be used to endorse or promote products  
-//       derived from this software without specific prior written permission. 
+//   nor the names of its contributors may be used to endorse or promote products  
+//   derived from this software without specific prior written permission. 
 // 
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
@@ -24,32 +24,28 @@
 // WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
 // POSSIBILITY OF SUCH DAMAGE. 
-// 
+//
 
-package org.objectledge;
+package org.objectledge.container;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
 /**
  *
- *
+ * <p>Created on Dec 16, 2003</p>
  * @author <a href="Rafal.Krzewski">rafal@caltha.pl</a>
- * @version $Id: ContainerTests.java,v 1.7 2003-12-17 10:04:03 fil Exp $
+ * @version $Id: AllTests.java,v 1.1 2003-12-17 10:03:54 fil Exp $
  */
-public class ContainerTests
+public class AllTests
 {
 
     public static Test suite()
     {
-        TestSuite suite = new TestSuite("Test for org.objectledge");
+        TestSuite suite = new TestSuite("Test for org.objectledge.container");
         //$JUnit-BEGIN$
-
+        suite.addTest(new TestSuite(LedgeContainerTest.class));
         //$JUnit-END$
-        suite.addTest(org.objectledge.configuration.AllTests.suite());
-        suite.addTest(org.objectledge.logging.AllTests.suite());
-        suite.addTest(org.objectledge.pico.AllTests.suite());
-        suite.addTest(org.objectledge.container.AllTests.suite());
         return suite;
     }
 }
