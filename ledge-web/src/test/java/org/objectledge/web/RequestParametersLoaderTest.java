85d92c1a01355c5735c9d4c8bef317659df215a9
Paweł Potempski <pawel.potempski@caltha.pl>
2003-12-29 09:46:13 +0000

    test helper valve added

---
+++ ledge-web/src/test/java/org/objectledge/web/RequestParametersLoaderTest.java
@@ -0,0 +1,80 @@
+// 
+// Copyright (c) 2003, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+// All rights reserved. 
+// 
+// Redistribution and use in source and binary forms, with or without modification,  
+// are permitted provided that the following conditions are met: 
+//  
+// * Redistributions of source code must retain the above copyright notice,  
+//   this list of conditions and the following disclaimer. 
+// * Redistributions in binary form must reproduce the above copyright notice,  
+//   this list of conditions and the following disclaimer in the documentation  
+//   and/or other materials provided with the distribution. 
+// * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
+//   nor the names of its contributors may be used to endorse or promote products  
+//   derived from this software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+// POSSIBILITY OF SUCH DAMAGE. 
+// 
+
+package org.objectledge.web;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.objectledge.context.Context;
+
+/**
+ * Pipeline processing valve that loads parameters into the context.
+ *
+ * @author <a href="mailto:pablo@caltha.pl">Pawel Potempski</a>
+ * @version $Id: RequestParametersLoaderTest.java,v 1.1 2003-12-29 09:46:13 pablo Exp $
+ */
+public class RequestParametersLoaderTest implements Runnable
+{
+	/** the context */
+	private Context context;
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param context the context.
+	 */
+	public RequestParametersLoaderTest(Context context)
+	{
+		this.context = context;		
+	}
+	
+    /**
+     * Run the pipeline valve - parse and load the parameters into the context.
+     */
+    public void run()
+    {
+    	RequestParameters parameters = (RequestParameters)context.
+    		getAttribute(RequestParameters.CONTEXT_KEY);
+		HttpContext httpContext = (HttpContext)context.getAttribute(HttpContext.CONTEXT_KEY);
+		HttpServletResponse response = httpContext.getResponse();    		
+		response.setStatus(200);
+		response.setContentType("text");
+		try
+		{
+			PrintWriter pw = response.getWriter();
+			pw.print(parameters.get("foo","bar"));
+			pw.close();
+		}
+		catch(Exception e)
+		{
+			throw new PipelineProcessingException("",e);
+		}
+    }
+}
