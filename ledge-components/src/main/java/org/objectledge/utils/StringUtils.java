2029d03243787cf51960057cbc7e645b232c14b4
Paweł Potempski <pawel.potempski@caltha.pl>
2003-12-30 14:35:10 +0000

    some new methods added

--- ledge-components/src/main/java/org/objectledge/utils/StringUtils.java
+++ ledge-components/src/main/java/org/objectledge/utils/StringUtils.java
@@ -28,72 +28,187 @@
 
 package org.objectledge.utils;
 
+import java.util.Locale;
+import java.util.StringTokenizer;
+
 /**
  * This class contains various functions for manipulating Java Strings.
  *
  * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
  * @author <a href="mailto:pablo@caltha.pl">Pawel Potempski</a>
  *
- * @version $Id: StringUtils.java,v 1.2 2003-12-03 14:39:43 mover Exp $
+ * @version $Id: StringUtils.java,v 1.3 2003-12-30 14:35:10 pablo Exp $
  */
 public class StringUtils
 {
-	/** private constructor */
-	private StringUtils()
-	{
-	}
-	
+    /** private constructor */
+    private StringUtils()
+    {
+    }
+
+    /**
+     * Expand unicode escapes.
+     * 
+     * @param s the string to process.
+     * @return processed string.
+     * @throws IllegalArgumentException if the string contains invalid unicode
+     *   escapes.
+     */
+    public static String expandUnicodeEscapes(String s) throws IllegalArgumentException
+    {
+        StringBuffer buffer = new StringBuffer();
+        int last = 0;
+        int cur = s.indexOf("\\u", last);
+        if (cur >= 0)
+        {
+            if (cur <= s.length() - 6)
+            {
+                buffer.setLength(0);
+                while (cur >= 0)
+                {
+                    buffer.append(s.substring(last, cur));
+                    String ucodeStr = s.substring(cur + 2, cur + 6);
+                    try
+                    {
+                        int ucode = Integer.parseInt(ucodeStr, 16);
+                        if (Character.isDefined((char)ucode))
+                        {
+                            buffer.append((char)ucode);
+                        }
+                        else
+                        {
+                            throw new IllegalArgumentException("invalid unicode character code "+
+                            									" in an unicode escape");
+                        }
+                    }
+                    catch (NumberFormatException e)
+                    {
+                        throw new IllegalArgumentException("invalid hexadecimal number " +
+                        									"in an unicode escape");
+                    }
+                    last = cur + 6;
+                    cur = s.indexOf("\\u", last);
+                }
+            }
+            else
+            {
+                throw new IllegalArgumentException("truncated unicode escape");
+            }
+        }
+        buffer.append(s.substring(last));
+        return buffer.toString();
+    }
+
+    /**
+     * Performs variable substitution on a template string.
+     *
+     * <p>The variables are formed of the dollar sign followed by a digit, ranging
+     * from 1 to 9. Special variable formed of the dollar singn followed by an
+     * asterisk is substitued with the remaining values separated by
+     * commas. Dollar sign followed by another dollar sign expands to single
+     * dollar sign. If there are not enough values provided, undefined variables
+     * will be substitutes with empty strings. Examples:
+     * <table>
+     * <tr><td>template</td><td>values</td><td>result</td></tr>
+     * <tr><td>$1 rules</td><td>{"ziu"}</td><td>ziu rules</td></tr>
+     * <tr><td>$1 said "$2"</td><td>{"Fred","Foo!"}</td><td>Fred said
+     * "Foo!"</td></tr>
+     * <tr><td>"$2" said $1</td><td>{"Fred","Foo!"}</td><td>"Foo!" said
+     * Fred</td></tr>
+     * <tr><td>$1 likes the following colors:
+     * $*</td><td>{"Mike","blue","grey","cyan"}</td>
+     * <td>Mike likes the following colors: blue, grey, cyan</td></tr>
+     * <tr><td>$1 likes the following $2:
+     * $*</td><td>{"Mike","food","pizza","french fries"}</td><td>Mike likes
+     * the following food: pizza, french fries</td></tr>
+     * </table></p>
+     *
+     * @param template the template string.
+     * @param values the values of the variables.
+     * @return the output string.
+     */
+    public static String substitute(String template, String[] values)
+    {
+        StringBuffer buff = new StringBuffer();
+        int maxUsed = 0;
+        char[] t = template.toCharArray();
+        for (int i = 0; i < t.length; i++)
+        {
+            if (t[i] == '$' && i < t.length - 1)
+            {
+                if (t[i + 1] == '$')
+                {
+                    buff.append('$');
+                    i++;
+                }
+                else if (t[i + 1] > '0' && t[i + 1] < '9')
+                {
+                    int v = t[i + 1] - '0';
+                    if (v - 1 < values.length)
+                    {
+                        buff.append(values[v - 1]);
+                    }
+                    if (v > maxUsed)
+                    {
+                        maxUsed = v;
+                    }
+                    i++;
+                }
+                else if (t[i + 1] == '*')
+                {
+                    if (maxUsed < values.length)
+                    {
+                        for (int v = maxUsed; v < values.length; v++)
+                        {
+                            buff.append(values[v]);
+                            buff.append(", ");
+                        }
+                        buff.setLength(buff.length() - 2);
+                    }
+                    i++;
+                }
+                else
+                {
+                    buff.append('$');
+                }
+            }
+            else
+            {
+                buff.append(t[i]);
+            }
+        }
+        return buff.toString();
+    }
+    
 	/**
-	 * Expand unicode escapes.
-	 * 
-	 * @param s the string to process.
-	 * @return processed string.
-	 * @throws IllegalArgumentException if the string contains invalid unicode
-	 *   escapes.
+	 * Build a locale from string.
+	 *
+	 * @param name a string representation of a locale
+	 * @return a <code>Locale</code> object
 	 */
-	public static String expandUnicodeEscapes(String s)
-		throws IllegalArgumentException
+	public static Locale getLocale(String name)
 	{
-		StringBuffer buffer = new StringBuffer();
-		int last = 0;
-		int cur = s.indexOf("\\u", last);
-		if(cur >= 0)
+		try
 		{
-			if(cur <= s.length() - 6)
+			Locale locale;
+			StringTokenizer st = new StringTokenizer(name, "_");
+			String language = st.nextToken();
+			String country = st.nextToken();
+			if(st.hasMoreTokens())
 			{
-				buffer.setLength(0);
-				while(cur >= 0)
-				{
-					buffer.append(s.substring(last,cur));
-					String ucodeStr = s.substring(cur+2, cur+6);
-					try
-					{
-						int ucode = Integer.parseInt(ucodeStr, 16);
-						if(Character.isDefined((char)ucode))
-						{
-							buffer.append((char)ucode);
-						}
-						else
-						{
-							throw new IllegalArgumentException("invalid unicode character code "+
-													 " in an unicode escape");
-						}
-					}
-					catch(NumberFormatException e)
-					{
-						throw new IllegalArgumentException("invalid hexadecimal number "+
-												 "in an unicode escape");
-					}
-					last = cur+6;
-					cur = s.indexOf("\\u", last);
-				}
+				String variant = st.nextToken();
+				locale = new Locale(language, country, variant);
 			}
 			else
 			{
-				throw new IllegalArgumentException("truncated unicode escape");
+				locale = new Locale(language, country);
 			}
+			return locale;
 		}
-		buffer.append(s.substring(last));
-		return buffer.toString();
+		catch(Exception e)
+		{
+			throw new IllegalArgumentException("Locale parsing exception - "
+												+"invalid string representation '"+name+"'");
+		}
 	}
 }
