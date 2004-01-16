66a60e7d99af7d02fe63adff71d11e53221d61b1
Rafal Krzewski <rafal.krzewski@caltha.pl>
2004-01-16 10:22:50 +0000

    make customized instance caching a sole reponsibility of the customized component provider. better separation of concerns, plus fixes a silly bug : )

--- ledge-container/src/main/java/org/objectledge/pico/customization/CustomizedComponentAdapter.java
+++ ledge-container/src/main/java/org/objectledge/pico/customization/CustomizedComponentAdapter.java
@@ -39,37 +39,31 @@ import org.picocontainer.defaults.NoSatisfiableConstructorsException;
  * An adapter for a component that is customized on behalf of a component that is asking for it.
  * 
  * <p>When another component is being initialized and it depends on the component that is managed 
- * by this adapter (identified by the componentKey), the customizedComponentContainer is consulted 
- * for a cached instance. If not present, the customizedContainerProvider is asked to produce an
- * instance of the customized component. Thus, different components may recieve different instnaces
- * of the managed component, depending on the customizedComponentProvider's semantics.</p>
+ * by this adapter (identified by the componentKey), the customizedContainerProvider is asked to 
+ * produce an instance of the customized component. Thus, different components may recieve different
+ * instnaces of the managed component, depending on the customizedComponentProvider's semantics.</p>
  *
  * @author <a href="Rafal.Krzewski">rafal@caltha.pl</a>
- * @version $Id: CustomizedComponentAdapter.java,v 1.12 2003-12-29 11:14:11 fil Exp $
+ * @version $Id: CustomizedComponentAdapter.java,v 1.13 2004-01-16 10:22:50 fil Exp $
  */
 public class CustomizedComponentAdapter
     implements ComponentAdapter
 {
     private Object componentKey;
 
-    private MutablePicoContainer customizedComponentContainer;
-    
     private CustomizedComponentProvider customizedComponentProvider;
     
     /**
      * Crates a new instance of the adapter.
      * 
      * @param componentKey the component key this adapter manages.
-     * @param customizedComponentContainer a container for storing customized components.
      * @param customizedComponentProvider a provider of customized components.
      */
     public CustomizedComponentAdapter(
         Object componentKey,
-        MutablePicoContainer customizedComponentContainer,
         CustomizedComponentProvider customizedComponentProvider)
     {
         this.componentKey = componentKey;
-        this.customizedComponentContainer = customizedComponentContainer;
         this.customizedComponentProvider = customizedComponentProvider;
     }
 
@@ -100,22 +94,9 @@ public class CustomizedComponentAdapter
         Object componentKey, Class componentImplementation)
         throws PicoInitializationException, PicoIntrospectionException, UnsupportedKeyTypeException
     {        
-        String marker = componentKey.toString();
-        if(componentKey instanceof Class)
-        {
-            marker = ((Class)componentKey).getName();
-        }
-        if(customizedComponentContainer.hasComponent(marker))
-        {
-            return customizedComponentContainer.getComponentInstance(marker);
-        }
-        else
-        {
-            ComponentAdapter adapter = customizedComponentProvider.
-                getCustomizedAdapter(dependencyContainer, componentKey, componentImplementation);
-            customizedComponentContainer.registerComponent(adapter);
-            return adapter.getComponentInstance(customizedComponentContainer);
-        }
+         ComponentAdapter adapter = customizedComponentProvider.
+             getCustomizedAdapter(componentKey, componentImplementation);
+         return adapter.getComponentInstance(dependencyContainer);
     }
 
     /**
