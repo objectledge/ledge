419d8599fcd87e4d5bb23b0c1031e5bbd4668519
Rafal Krzewski <rafal.krzewski@caltha.pl>
2004-01-19 13:04:05 +0000

    added NameSequenceFactory tests

--- ledge-web/src/test/java/org/objectledge/test/views/foo/Bar.java
+++ ledge-web/src/test/java/org/objectledge/test/views/foo/Bar.java
@@ -1,18 +1,18 @@
 // 
 // Copyright (c) 2003, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
 // All rights reserved. 
-//   
+// 
 // Redistribution and use in source and binary forms, with or without modification,  
 // are permitted provided that the following conditions are met: 
-//   
+//  
 // * Redistributions of source code must retain the above copyright notice,  
-//   this list of conditions and the following disclaimer. 
+//	 this list of conditions and the following disclaimer. 
 // * Redistributions in binary form must reproduce the above copyright notice,  
-//   this list of conditions and the following disclaimer in the documentation  
-//   and/or other materials provided with the distribution. 
+//	 this list of conditions and the following disclaimer in the documentation  
+//	 and/or other materials provided with the distribution. 
 // * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
-//   nor the names of its contributors may be used to endorse or promote products  
-//   derived from this software without specific prior written permission. 
+//	 nor the names of its contributors may be used to endorse or promote products  
+//	 derived from this software without specific prior written permission. 
 // 
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
@@ -24,30 +24,23 @@
 // WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
 // POSSIBILITY OF SUCH DAMAGE. 
-//
-
-package org.objectledge.web.mvc.finders;
-
-import junit.framework.Test;
-import junit.framework.TestSuite;
+// 
+package org.objectledge.test.views.foo;
 
 /**
- *
- * <p>Created on Dec 23, 2003</p>
- * @author <a href="Rafal.Krzewski">rafal@caltha.pl</a>
- * @version $Id: AllTests.java,v 1.3 2004-01-19 13:04:05 fil Exp $
+ * A test view.
+ *  
+ * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
+ * @version $Id: Bar.java,v 1.1 2004-01-19 13:04:05 fil Exp $
  */
-public class AllTests
+public class Bar implements Runnable
 {
-
-    public static Test suite()
+    /* (non-Javadoc)
+     * @see java.lang.Runnable#run()
+     */
+    public void run()
     {
-        TestSuite suite = new TestSuite("Test for org.objectledge.web");
-        //$JUnit-BEGIN$
-        suite.addTest(new TestSuite(ViewFallbackSequenceTest.class));
-        suite.addTest(new TestSuite(ViewLookupSequenceTest.class));
-        suite.addTest(new TestSuite(NameSequenceFactoryTest.class));
-        //$JUnit-END$
-        return suite;
+        // TODO Auto-generated method stub
+
     }
 }
---
+++ ledge-web/src/test/java/org/objectledge/web/mvc/finders/NameSequenceFactoryTest.java
@@ -0,0 +1,115 @@
+// 
+// Copyright (c) 2003, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+// All rights reserved. 
+// 
+// Redistribution and use in source and binary forms, with or without modification,  
+// are permitted provided that the following conditions are met: 
+//  
+// * Redistributions of source code must retain the above copyright notice,  
+//	 this list of conditions and the following disclaimer. 
+// * Redistributions in binary form must reproduce the above copyright notice,  
+//	 this list of conditions and the following disclaimer in the documentation  
+//	 and/or other materials provided with the distribution. 
+// * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
+//	 nor the names of its contributors may be used to endorse or promote products  
+//	 derived from this software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+// POSSIBILITY OF SUCH DAMAGE. 
+// 
+package org.objectledge.web.mvc.finders;
+
+import junit.framework.TestCase;
+
+import org.jcontainer.dna.Configuration;
+import org.objectledge.configuration.ConfigurationFactory;
+import org.objectledge.filesystem.FileSystem;
+import org.objectledge.logging.LoggerFactory;
+import org.objectledge.templating.Template;
+import org.objectledge.templating.Templating;
+import org.objectledge.templating.velocity.VelocityTemplating;
+import org.objectledge.test.views.foo.Bar;
+import org.objectledge.xml.XMLValidator;
+
+/**
+ * 
+ * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
+ * @version $Id: NameSequenceFactoryTest.java,v 1.1 2004-01-19 13:04:05 fil Exp $
+ */
+public class NameSequenceFactoryTest extends TestCase
+{
+    private FileSystem fs;
+    
+    private ConfigurationFactory configFactory;
+    
+    /**
+     * Constructor for NameSequenceFactoryTest.
+     * @param arg0
+     */
+    public NameSequenceFactoryTest(String arg0)
+        throws Exception
+    {
+        super(arg0);
+        String root = System.getProperty("ledge.root");
+        if(root == null)
+        {
+            throw new Exception("system property ledge.root undefined. "+
+            "use -Dledge.root=.../ledge-container/src/test/resources");
+        }
+        fs = FileSystem.getStandardFileSystem(root+"/view-sequences");
+        XMLValidator validator = new XMLValidator();        
+        configFactory = new ConfigurationFactory(fs, validator, ".");
+    }
+    
+    public void testLookups()
+        throws Exception
+    {
+        NameSequenceFactory factory = getNameSequenceFactory("standard");
+        Sequence classSequence = factory.getClassNameSequence("views.foo.Bar");
+        assertEquals("org.objectledge.test.views.foo.Bar", classSequence.next());
+        assertEquals("org.objectledge.test.views.foo.Default", classSequence.next());
+        assertEquals("org.objectledge.test.views.Default", classSequence.next());
+        assertEquals("org.objectledge.test.Default", classSequence.next());
+        assertEquals(false, classSequence.hasNext());
+        Sequence templateSequence = factory.getTemplateNameSequence("views.foo.Bar");
+        assertEquals("views/foo/Bar", templateSequence.next());
+        assertEquals("views/foo/Default", templateSequence.next());
+        assertEquals("views/Default", templateSequence.next());
+        assertEquals("Default", templateSequence.next());
+        assertEquals(false, templateSequence.hasNext());
+    }
+    
+    public void testReverseLookups()
+        throws Exception
+    {
+        NameSequenceFactory factory = getNameSequenceFactory("standard");
+        assertEquals("views.foo.Bar", factory.getView(Bar.class));
+        Templating templating = getTemplating();
+        Template barTemplate = templating.getTemplate("views/foo/Bar");
+        assertEquals("views.foo.Bar", factory.getView(barTemplate));
+    }
+    
+    public NameSequenceFactory getNameSequenceFactory(String configVariant)
+        throws Exception
+    {
+        Configuration config = configFactory.
+            getConfig("org.objectledge.web.mvc.finders.NameSequenceFactory:"+configVariant, 
+                NameSequenceFactory.class);
+        return new NameSequenceFactory(config);
+    }
+    
+    public Templating getTemplating()
+    {
+        Configuration config = configFactory.getConfig(Templating.class, VelocityTemplating.class);
+        LoggerFactory loggerFactory = new LoggerFactory();
+        return new VelocityTemplating(config, loggerFactory.getLogger(Templating.class), fs);
+    }
+}
