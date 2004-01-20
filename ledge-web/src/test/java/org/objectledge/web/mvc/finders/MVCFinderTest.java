46e8425ce2b655ec1ecdcd56e97d29bb3b1cc7b0
Rafal Krzewski <rafal.krzewski@caltha.pl>
2004-01-20 11:23:20 +0000

    added MVCFinder test

---
+++ ledge-web/src/test/java/org/objectledge/web/mvc/finders/MVCFinderTest.java
@@ -0,0 +1,140 @@
+// 
+// Copyright (c) 2003, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+// All rights reserved. 
+// 
+// Redistribution and use in source and binary forms, with or without modification,  
+// are permitted provided that the following conditions are met: 
+//  
+// * Redistributions of source code must retain the above copyright notice,  
+//	 this list of conditions and the following disclaimer. 
+// * Redistributions in binary form must reproduce the above copyright notice,  
+//	 this list of conditions and the following disclaimer in the documentation  
+//	 and/or other materials provided with the distribution. 
+// * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
+//	 nor the names of its contributors may be used to endorse or promote products  
+//	 derived from this software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+// POSSIBILITY OF SUCH DAMAGE. 
+// 
+package org.objectledge.web.mvc.finders;
+
+import junit.framework.TestCase;
+
+import org.jcontainer.dna.Configuration;
+import org.jcontainer.dna.Logger;
+import org.objectledge.configuration.ConfigurationFactory;
+import org.objectledge.context.Context;
+import org.objectledge.filesystem.FileSystem;
+import org.objectledge.logging.LoggerFactory;
+import org.objectledge.templating.Template;
+import org.objectledge.templating.Templating;
+import org.objectledge.templating.velocity.VelocityTemplating;
+import org.objectledge.web.mvc.builders.DefaultTemplate;
+import org.objectledge.xml.XMLValidator;
+import org.picocontainer.MutablePicoContainer;
+import org.picocontainer.defaults.DefaultPicoContainer;
+
+/**
+ * 
+ * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
+ * @version $Id: MVCFinderTest.java,v 1.1 2004-01-20 11:23:20 fil Exp $
+ */
+public class MVCFinderTest extends TestCase
+{
+    private MVCFinder finder;
+    
+    private Templating templating;
+    
+    /**
+     * Constructor for MVCFinderTest.
+     * @param arg0
+     */
+    public MVCFinderTest(String arg0)
+    {
+        super(arg0);
+    }
+
+    public void setUp()
+        throws Exception
+    {
+        String root = System.getProperty("ledge.root");
+        if(root == null)
+        {
+            throw new Exception("system property ledge.root undefined. "+
+            "use -Dledge.root=.../ledge-container/src/test/resources");
+        }
+        FileSystem fs = FileSystem.getStandardFileSystem(root+"/finder");
+        XMLValidator validator = new XMLValidator();        
+        ConfigurationFactory configFactory = new ConfigurationFactory(fs, validator, ".");
+        Configuration config = configFactory.
+            getConfig(NameSequenceFactory.class, NameSequenceFactory.class);
+        NameSequenceFactory nameSequenceFactory = new NameSequenceFactory(config);
+        config = configFactory.getConfig(Templating.class, VelocityTemplating.class);
+        LoggerFactory loggerFactory = new LoggerFactory();
+        Logger logger = loggerFactory.getLogger(Templating.class);
+        templating = new VelocityTemplating(config, logger, fs);
+        logger = loggerFactory.getLogger(MVCFinder.class);
+        MutablePicoContainer container = new DefaultPicoContainer();
+        container.registerComponentImplementation(Context.class); 
+        finder = new MVCFinder(container, logger, templating, nameSequenceFactory);
+    }
+
+    public void testFindBuilderTemplate()
+    {
+        Template template = finder.findBuilderTemplate("foo.Bar");
+        assertEquals( "views/foo/Bar", template.getName());
+    }
+
+    public void testFindEnclosingBuilderTemplate()
+    {
+        Template template = finder.findBuilderTemplate("foo.Bar");
+        assertEquals("views/foo/Bar", template.getName());
+        template = finder.findEnclosingBuilderTemplate(template);
+        assertEquals("views/foo/Default", template.getName());
+        template = finder.findEnclosingBuilderTemplate(template);
+        assertEquals("views/Default", template.getName());
+        template = finder.findEnclosingBuilderTemplate(template);
+        assertEquals(DefaultTemplate.class, template.getClass());
+    }
+
+    /*
+     * Test for String findViewName(Template)
+     */
+    public void testFindViewNameTemplate()
+    {
+        Template template = finder.findBuilderTemplate("foo.Bar");
+        assertEquals("foo.Bar", finder.findViewName(template));
+    }
+
+    public void testFindAction()
+    {
+        //TODO Implement findAction().
+    }
+
+    public void testFindBuilder()
+    {
+        //TODO Implement findBuilder().
+    }
+
+    public void testFindEnclosingBuilder()
+    {
+        //TODO Implement findEnclosingBuilder().
+    }
+
+    /*
+     * Test for String findViewName(Builder)
+     */
+    public void testFindViewNameBuilder()
+    {
+        //TODO Implement findViewName().
+    }
+}
