d17526106c7532a54bd8a89cfb795d4cbc0d1549
Damian Gajda <damian.gajda@caltha.pl>
2004-02-03 13:47:49 +0000

    Added ASF license

--- ledge-components/src/main/java/org/objectledge/encodings/EncodingMap.java
+++ ledge-components/src/main/java/org/objectledge/encodings/EncodingMap.java
@@ -33,10 +33,60 @@ import java.util.HashMap;
 /**
  * EncodingMap is a convenience class which handles conversions between IANA encoding names and Java
  * encoding names, and vice versa.
- * Data derived from Xerces-J 2 <code>org.apache.xerces.util.EncodingMap</code> class.
+ * <p>Data derived from Xerces-J 2 <code>org.apache.xerces.util.EncodingMap</code> class.
+ * As required, license attached below:
+ * </p>
+ * <pre>
+ * The Apache Software License, Version 1.1
  *
+ *
+ * Copyright (c) 1999-2002 The Apache Software Foundation.  
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:  
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Xerces" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written 
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * </pre>
+ * 
  * @author    <a href="mailto:dgajda@caltha.pl">Damian Gajda</a>
- * @version   $Id: EncodingMap.java,v 1.1 2004-02-02 18:59:01 zwierzem Exp $
+ * @version   $Id: EncodingMap.java,v 1.2 2004-02-03 13:47:49 zwierzem Exp $
  */
 public class EncodingMap
 {
