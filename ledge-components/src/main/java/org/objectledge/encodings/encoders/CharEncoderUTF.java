f1a25fa0f2118db07bff5deff2e4f4ac2f4cd012
Damian Gajda <damian.gajda@caltha.pl>
2004-02-03 13:47:18 +0000

    Made this class abstract

--- ledge-components/src/main/java/org/objectledge/encodings/encoders/CharEncoderUTF.java
+++ ledge-components/src/main/java/org/objectledge/encodings/encoders/CharEncoderUTF.java
@@ -4,9 +4,9 @@ package org.objectledge.encodings.encoders;
  * Base encoder for UTF (Unicode) character sets.
  *
  * @author <a href="mailto:dgajda@caltha.pl">Damian Gajda</a>
- * @version $Id: CharEncoderUTF.java,v 1.1 2004-02-02 18:59:00 zwierzem Exp $
+ * @version $Id: CharEncoderUTF.java,v 1.2 2004-02-03 13:47:18 zwierzem Exp $
  */
-public class CharEncoderUTF extends CharEncoder
+public abstract class CharEncoderUTF extends CharEncoder
 {
     private char[] res = new char[1];
 
