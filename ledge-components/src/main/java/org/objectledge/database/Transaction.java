194fa37300b1ceab33790cd8566250b90b1363dd
Rafal Krzewski <rafal.krzewski@caltha.pl>
2004-02-06 15:37:55 +0000

    added trasaction gestures tracing, and unifished transaction guard valve

--- ledge-components/src/main/java/org/objectledge/database/Transaction.java
+++ ledge-components/src/main/java/org/objectledge/database/Transaction.java
@@ -34,25 +34,47 @@ import javax.transaction.TransactionManager;
 import javax.transaction.UserTransaction;
 
 import org.jcontainer.dna.Logger;
+import org.objectledge.context.Context;
+import org.objectledge.pipeline.ProcessingException;
+import org.objectledge.pipeline.Valve;
+import org.objectledge.utils.StringUtils;
 
 /**
  * Helps dealing with transactions in the application code.
  * 
  * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
- * @version $Id: Transaction.java,v 1.3 2004-02-06 08:41:57 fil Exp $
+ * @version $Id: Transaction.java,v 1.4 2004-02-06 15:37:55 fil Exp $
  */
 public abstract class Transaction
 {
+    /** {@link Conext} key under where tracing buffer is kept. */ 
+    public static final String TRACE_BUFFER = 
+        "org.objectledge.database.Transaction.traceBuffer";
+
+    /** {@link Conext} key under where nesting counter is kept. */ 
+    public static final String NESTING_COUNTER = 
+        "org.objectledge.database.Transaction.nestingCounter";
+
     /** the logger. */
     protected Logger log;
+
+    /** Context the context. */
+    private Context context;
+        
+    /** The tracing depth. */
+    private int tracing;        
 
     /**
      * Constructs a Transaction component.
      * 
+     * @param tracing tracing depth.
+     * @param context the threads processing context.
      * @param log the logger to use for error reporting.
      */
-    protected Transaction(Logger log)
+    protected Transaction(int tracing, Context context, Logger log)
     {
+        this.tracing = tracing;
+        this.context = context;
         this.log = log;
     }
     
@@ -83,6 +105,7 @@ public abstract class Transaction
     public boolean begin()
         throws SQLException
     {
+        trace("begin");  
         boolean controler;
         try 
         {
@@ -107,7 +130,7 @@ public abstract class Transaction
                 log.error("failed to begin transaction", e);
                 throw (SQLException)new SQLException("failed to begin transaction").initCause(e);
             }
-        }  
+        }
         return controler;
     }
     
@@ -120,6 +143,7 @@ public abstract class Transaction
     public void commit(boolean controler)
         throws SQLException
     {
+        trace("commit");
         if(controler)
         {
             try
@@ -143,6 +167,7 @@ public abstract class Transaction
     public void rollback(boolean controler)
         throws SQLException
     {
+        trace("rollback");
         if(controler)
         {
             try
@@ -166,6 +191,154 @@ public abstract class Transaction
                 log.error("setRollbackOnly failed", e);
                 throw (SQLException)new SQLException("setRollbackOnly failed").initCause(e);
             }
+        }
+    }
+
+    /**
+     * A valve that checks if the thread finished any transactions it might have started.
+     */    
+    public static class GuardValve
+        implements Valve
+    {
+        /** The transaction wrapper object. */
+        private Transaction transaction;
+        
+        /** The logger. */
+        private Logger log;
+        
+        /**
+         * Creates an instance of the GuardValve.
+         * 
+         * @param transaction the Transaction.
+         * @param log the logger.
+         */
+        public GuardValve(Transaction transaction, Logger log)
+        {
+            this.transaction = transaction;
+            this.log = log;
+        }
+        
+        /** 
+         * {@inheritDoc}
+         */
+        public void process(Context context) throws ProcessingException
+        {
+            transaction.cleanupState(context, log);
+        }
+    }
+    
+    // implementation ///////////////////////////////////////////////////////////////////////////
+    
+    private void cleanupState(Context context, Logger log)
+    {
+        try
+        {
+            int status = getUserTransaction().getStatus();
+            if(!(status == Status.STATUS_NO_TRANSACTION ||
+                status == Status.STATUS_COMMITTED ||
+                status == Status.STATUS_ROLLEDBACK))
+            {
+                log.error("Thread owns an active transaction.\n"+
+                    getTrace(context)+
+                    "Rolling the transaction back.");
+                try
+                {
+                    getUserTransaction().rollback();
+                }
+                catch(Exception e)
+                {
+                    log.error("rollaback failed", e);
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            log.error("failed to check transaction state", e);
+        }
+    }
+    
+    private static String getTrace(Context context)
+    {
+        StringBuffer trace = (StringBuffer)context.getAttribute(TRACE_BUFFER);
+        if(trace != null)
+        {
+            return trace.toString();
+        }
+        else
+        {
+            return "Set tracing parameter to 1 or more to see the places "+
+                "where transaction events occured.\n";
+        }
+    }
+    
+    private void trace(String op)
+    {
+        if(tracing > 0)
+        {
+            StringBuffer trace = (StringBuffer)context.getAttribute(TRACE_BUFFER);
+            Counter nestingCounter = (Counter)context.getAttribute(NESTING_COUNTER);
+            if(nestingCounter == null)
+            {
+                nestingCounter = new Counter();
+                context.setAttribute(NESTING_COUNTER, nestingCounter);
+            }
+            if(op.equals("begin"))
+            {
+                nestingCounter.inc();
+            }
+            else
+            {
+                nestingCounter.dec();
+            }
+            if(trace == null)
+            {
+                trace = new StringBuffer();
+                context.setAttribute(TRACE_BUFFER, trace);
+            }
+            StringUtils.indent(trace, (nestingCounter.get()-1)*2).append(op).append(" at\n");
+            StackTraceElement[] frames = new Exception().getStackTrace();
+            int start = 0;
+            for(int i=0; i<frames.length; i++)
+            {
+                if(frames[i].getMethodName().equals(op))
+                {
+                    start = i;
+                    break;
+                }
+            }
+            for(int i=start+1; i<frames.length && i<=start+tracing; i++)
+            {
+                StringUtils.indent(trace, (nestingCounter.get()-1)*2).
+                    append(frames[i].toString()).append('\n');
+            }
+        }
+    }
+    
+    /**
+     * Tiny helper class.
+     */
+    private static class Counter
+    {
+        private int count;
+        
+        public Counter()
+        {
+            count = 0;
+        }
+        
+        public void inc()
+        {
+            count++;
+        }
+        
+        public void dec()
+        {
+            count--;
+        }
+        
+        public int get()
+        {
+            return count;
         }
     }
 }
