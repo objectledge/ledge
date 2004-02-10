be19726d45316c213ea2ffe70cfc5f16edbfc6db
Rafal Krzewski <rafal.krzewski@caltha.pl>
2004-02-10 11:16:59 +0000

    debugging

--- CVSROOT/logcheck.sh
+++ CVSROOT/logcheck.sh
@@ -11,11 +11,13 @@ cat > $TEMPFILE
 #
 # Invoke spackle
 #
+echo $CVSROOT/CVSROOT/log_accum.pl $USER `echo ${1} | cut -d , -f 1` < $TEMPFILE
 $CVSROOT/CVSROOT/log_accum.pl $USER `echo ${1} | cut -d , -f 1` < $TEMPFILE
 
 #
 # Invoke cvsspam
 #
+echo $CVSROOT/CVSROOT/collect_diffs.rb --from $USER --to objectledge-cvshtml@lists.caltha.pl ${1} < $TEMPFILE
 $CVSROOT/CVSROOT/collect_diffs.rb --from $USER --to objectledge-cvshtml@lists.caltha.pl ${1} < $TEMPFILE
 
 #
