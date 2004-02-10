1cb7e0f6daf0feb3ff9c961313e4449750fd505c
Rafal Krzewski <rafal.krzewski@caltha.pl>
2004-02-10 11:38:57 +0000

    mucking around

--- CVSROOT/commitcheck.sh
+++ CVSROOT/commitcheck.sh
@@ -12,14 +12,17 @@ if test "x-$ENABLE_ACLS" = "x-yes" ; then
     $CVSROOT/CVSROOT/cvs_acls.pl ${1+"$@"} || exit 1
 fi
 
+TMPFILE=/tmp/cvs.$$
+cat > $TMPFILE
+
 #
 # Tests pass, make the patch ready to commit and report.
 #
-$CVSROOT/CVSROOT/commit_prep.pl ${1+"$@"} || exit 2
+$CVSROOT/CVSROOT/commit_prep.pl ${1+"$@"} < $TMPFILE || exit 2
 
 #
 # Bridge to cvsspam
 #
-$CVSROOT/CVSROOT/record_lastdir.rb ${1+"$@"} || exit 2
+$CVSROOT/CVSROOT/record_lastdir.rb ${1+"$@"} < $TMPFILE || exit 2
 
 exit 0
