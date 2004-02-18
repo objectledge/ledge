d9ae83756835529a5b95b0097ed8c8e33489b8af
Rafal Krzewski <rafal.krzewski@caltha.pl>
2004-02-18 11:45:54 +0000

    sketching new authentication system

---
+++ ledge-components/src/main/java/org/objectledge/authentication/NamingPolicy.java
@@ -0,0 +1,404 @@
+// 
+// Copyright (c) 2003, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+// All rights reserved. 
+// 
+// Redistribution and use in source and binary forms, with or without modification,  
+// are permitted provided that the following conditions are met: 
+//  
+// * Redistributions of source code must retain the above copyright notice,  
+//	 this list of conditions and the following disclaimer. 
+// * Redistributions in binary form must reproduce the above copyright notice,  
+//	 this list of conditions and the following disclaimer in the documentation  
+//	 and/or other materials provided with the distribution. 
+// * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
+//	 nor the names of its contributors may be used to endorse or promote products  
+//	 derived from this software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+// POSSIBILITY OF SUCH DAMAGE. 
+// 
+package org.objectledge.authentication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Properties;
+
+import javax.naming.CompoundName;
+import javax.naming.InvalidNameException;
+import javax.naming.Name;
+
+import org.jcontainer.dna.Configuration;
+import org.jcontainer.dna.ConfigurationException;
+import org.objectledge.parameters.Parameters;
+
+/**
+ * Specifies a policy of naming accounts in the system.
+ * 
+ * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
+ * @version $Id: NamingPolicy.java,v 1.1 2004-02-18 11:45:54 fil Exp $
+ */
+public class NamingPolicy
+{
+    private String loginProperty;
+    
+    private Properties syntax = new Properties();
+    
+    private Token[] tokens;
+    
+    /**
+     * Creates an instance of the NamingPolicyComponent.
+     * 
+     * @param config the configuration.
+     * @throws ConfigurationException if the configuration is invalid.
+     */
+    public NamingPolicy(Configuration config)
+        throws ConfigurationException
+    {
+        loginProperty = config.getChild("login-property").getAttribute("name", null);
+        if(loginProperty == null)
+        { 
+           loginProperty = config.getChild("login-property").getValue(null);
+        }
+        
+        Configuration syntaxConfig = config.getChild("syntax");
+        Configuration tokensConfig = config.getChild("tokens");
+        if(config.getChild("syntax").getChild("ldap-syntax", false) != null)
+        {
+            syntax.put("jndi.syntax.direction", "right_to_left");
+            syntax.put("jndi.syntax.separator", ",");
+            syntax.put("jndi.syntax.separator.ava", ",");
+            syntax.put("jndi.syntax.separator.typeval", "=");
+            syntax.put("jndi.syntax.ignorecase", "true");
+            syntax.put("jndi.syntax.escape", "\\");
+        }
+        else
+        {
+            Configuration[] syntaxProperties = syntaxConfig.getChildren("property");
+            for(int i=0; i<syntaxProperties.length; i++)
+            {
+                syntax.put(syntaxProperties[i].getAttribute("name"), 
+                    syntaxProperties[i].getValue(syntaxProperties[i].getAttribute("value", null)));
+            }
+        }
+        Configuration[] tokenConfig = tokensConfig.getChildren("token");
+        tokens = new Token[tokenConfig.length];
+        boolean loginFound = false;
+        for(int i=0; i<tokenConfig.length; i++)
+        {
+            tokens[i] = new Token(tokenConfig[i]);
+            if(tokens[i].specifies(loginProperty))
+            {
+                loginFound = true;
+            }
+        }
+        if(tokens.length > 1 && syntax.get("jndi.syntax.separator") == null)
+        {
+            throw new ConfigurationException("multiple tokens defined, but syntax does not "+
+                "specify jndi.syntax.separator", 
+                 syntaxConfig.getPath(), syntaxConfig.getLocation());
+        }
+        if(!loginFound)
+        {
+            throw new ConfigurationException("no token specifies "+loginProperty+" property", 
+                 tokensConfig.getPath(), tokensConfig.getLocation());
+        }
+    }
+
+    /**
+     * Returns a distinguished name constructed from provided parameters in conformance to 
+     * configured syntax.
+     * 
+     * @param parameters the parameters to compose name of.
+     * @return the distinguished name.
+     */    
+    public String getDn(Parameters parameters)
+    {
+        StringBuffer target = new StringBuffer();
+        String sep = (String)syntax.get("jndi.syntax.separator");
+        for(int i=0; i<tokens.length; i++)
+        {
+            tokens[i].render(parameters, target);
+            if(i<tokens.length-1)
+            {
+                target.append(sep);
+            }
+        }
+        return target.toString();
+    }
+    
+    /**
+     * Retrieves the login name from the distinguished name.
+     * 
+     * @param dn the distinguished name.
+     * @return the login name.
+     * @throws InvalidNameException if the name does not conform to the defined syntax.
+     */
+    public String getLogin(String dn)
+        throws InvalidNameException
+    {
+        Name name = new CompoundName(dn, syntax);
+        if(name.size() != tokens.length)
+        {
+            throw new InvalidNameException("invalid name, expecting "+tokens.length+" elements");
+        }
+        for(int i=0; i<name.size(); i++)
+        {
+            if(tokens[i].specifies(loginProperty))
+            {
+                return tokens[i].get(name.get(i), loginProperty);
+            }
+            else
+            {
+                if(!tokens[i].match(name.get(i)))
+                {
+                    throw new InvalidNameException("invalied name, element "+name.get(i)+
+                        " does not match "+tokens[i].toString());
+                }
+            }
+        }
+        ///CLOVER:ON
+        throw new IllegalStateException();
+        ///CLOVER:OFF
+    }
+
+    /**
+     * Represents syntax of a compound disthinguished name element. 
+     * 
+     * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
+     * @version $Id: NamingPolicy.java,v 1.1 2004-02-18 11:45:54 fil Exp $
+     */    
+    public static class Token
+    {
+        /** constant string elements, first and last may be null. */
+        private String[] strings;
+        
+        /** embedded property names, alwas has string.length - 1 elements. */
+        private String[] properties;
+        
+        /**
+         * Creates token instance from a configuration element.
+         * 
+         * @param config the configuration element.
+         * @throws ConfigurationException if the configuration is invalid.
+         */
+        public Token(Configuration config)
+            throws ConfigurationException
+        {
+            Configuration[] elements = config.getChildren();
+            if(elements.length == 0)
+            {
+                throw new ConfigurationException("at least one element required", config.getPath(),
+                    config.getLocation());
+            }
+            List stringSpecs = new ArrayList(elements.length/2);
+            List propertySpecs = new ArrayList(elements.length/2);
+            for(int i=0; i<elements.length; i++)
+            {
+                if(elements[i].getName().equals("string"))
+                {
+                    String value = elements[i].getAttribute("value", null);
+                    if(value == null)
+                    {
+                        value = elements[i].getValue();
+                    }
+                    stringSpecs.add(value);
+                }
+                else
+                {
+                    if(stringSpecs.size() == 0)
+                    {
+                        stringSpecs.add(null);
+                    }
+                    String name = elements[i].getAttribute("name", null);
+                    if(name == null)
+                    {
+                        name = elements[i].getValue();
+                    }
+                    propertySpecs.add(name);
+                }
+            }
+            if(stringSpecs.size() == propertySpecs.size())
+            {
+                stringSpecs.add(null);
+            }
+            strings = new String[stringSpecs.size()];
+            stringSpecs.toArray(strings);
+            properties = new String[propertySpecs.size()];
+            propertySpecs.toArray(properties);
+        }
+        
+        /**
+         * Renders token image based on provided parameters into an output buffer.
+         * 
+         * @param parameters the parameters to use for rendering.
+         * @param target the output buffer.
+         */
+        public void render(Parameters parameters, StringBuffer target)
+        {
+            for(int i=0; i<strings.length + properties.length; i++)
+            {
+                if(i%2 == 0)
+                {
+                    if(strings[i/2] != null)
+                    {
+                        target.append(strings[i/2]);
+                    }
+                }
+                else
+                {
+                    String value = parameters.get(properties[i/2+1]);
+                    if(value != null)
+                    { 
+                        target.append(value);
+                    }
+                    else
+                    {
+                        throw new NoSuchElementException("undefined property "+properties[i/2+1]);
+                    }
+                }
+            }
+        }
+
+        /**
+         * Checks if the given token image matches the specified syntax.
+         * 
+         * @param image the token image.
+         * @return <code>true</code> if the given token image matches the specified syntax.
+         */        
+        public boolean match(String image)
+        {
+            int lastPos = 0;
+            for(int i=0; i<strings.length; i++)
+            {
+                if(strings[i] != null)
+                {
+                    int pos = image.indexOf(strings[i], lastPos);
+                    if(pos >= 0)
+                    {
+                        lastPos = pos+strings[i].length();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        /**
+         * Retrieves a property from given token image.
+         * 
+         * @param image the token image.
+         * @param propertyName the property name.
+         * @return the property value.
+         * @throws IllegalArgumentException if the token does not specify this property.
+         * @throws InvalidNameException if the token does not conform to defined syntax.
+         */        
+        public String get(String image, String propertyName)
+            throws IllegalArgumentException, InvalidNameException
+        {
+            int lastPos = 0;
+            int nextPos = image.length();
+            for(int i=0; i<strings.length+properties.length; i++)
+            {
+                if(i%2 == 0)
+                {
+                    if(strings[i/2] != null)
+                    {
+                        int pos = image.indexOf(strings[i/2], lastPos);
+                        if(pos >= 0)
+                        {
+                            lastPos = pos+strings[i/2].length();
+                        }
+                        else
+                        {
+                            throw new InvalidNameException("invalid token "+image+" "+
+                                strings[i/2+1]+" is missing");
+                        }
+                    }
+                    if(i/2+1 < strings.length)
+                    {
+                        if(strings[i/2+1] != null)
+                        {
+                            int pos = image.indexOf(strings[i/2+1], lastPos);
+                            if(pos >= 0)
+                            {
+                                nextPos = pos;
+                            }
+                            else
+                            {
+                                throw new InvalidNameException("invalid token "+image+" "+
+                                    strings[i/2+1]+" is missing");
+                            }
+                        }
+                        else
+                        {
+                            nextPos = image.length();
+                        }
+                    }
+                }
+                else
+                {
+                    if(properties[i/2+1].equals(propertyName))
+                    {
+                        return image.substring(lastPos, nextPos);
+                    }
+                }
+            }
+            throw new IllegalArgumentException("token does not specify "+propertyName+" property");
+        }
+    
+        /**
+         * Checks if this token specifies a property.
+         * 
+         * @param propertyName the name of the property.
+         * @return <code>true</code> if this token specifies a property.
+         */        
+        public boolean specifies(String propertyName)
+        {
+            for(int i=0; i<properties.length; i++)
+            {
+                if(properties[i].equals(propertyName))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        /**
+         * Returns a string representation of the token's syntax.
+         * 
+         * @return a string representation of the token's syntax.
+         */
+        public String toString()
+        {
+            StringBuffer target = new StringBuffer();
+            for(int i=0; i<strings.length + properties.length; i++)
+            {
+                if(i%2 == 0)
+                {
+                    if(strings[i/2] != null)
+                    {
+                        target.append(strings[i/2]);
+                    }
+                }
+                else
+                {
+                    target.append('<').append(properties[i/2+1]).append('>');
+                }
+            }
+            return target.toString();            
+        }
+    }
+}
---
+++ ledge-components/src/main/java/org/objectledge/authentication/UserManager.java
@@ -0,0 +1,271 @@
+// 
+// Copyright (c) 2003, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+// All rights reserved. 
+// 
+// Redistribution and use in source and binary forms, with or without modification,  
+// are permitted provided that the following conditions are met: 
+//  
+// * Redistributions of source code must retain the above copyright notice,  
+//	 this list of conditions and the following disclaimer. 
+// * Redistributions in binary form must reproduce the above copyright notice,  
+//	 this list of conditions and the following disclaimer in the documentation  
+//	 and/or other materials provided with the distribution. 
+// * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
+//	 nor the names of its contributors may be used to endorse or promote products  
+//	 derived from this software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+// POSSIBILITY OF SUCH DAMAGE. 
+// 
+package org.objectledge.authentication;
+
+import java.security.Principal;
+
+import javax.naming.InvalidNameException;
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+
+import org.objectledge.naming.ContextFactory;
+import org.objectledge.parameters.Parameters;
+
+/**
+ * A base implementation of the UserManager interface.
+ * 
+ * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
+ * @version $Id: UserManager.java,v 1.1 2004-02-18 11:45:54 fil Exp $
+ */
+public abstract class UserManager
+{
+    // instance variables ///////////////////////////////////////////////////////////////////////
+    
+    /** the naming policy to be used. */
+    protected NamingPolicy namingPolicy;
+    
+    /** the login verifier to be used. */
+    protected LoginVerifier loginVerifier;
+    
+    /** the password digester to be used. */
+    protected PasswordDigester passwordDigester;
+    
+    /** the password generator to be used. */
+    protected PasswordGenerator passwordGenerator;
+    
+    /** the directory context. */
+    protected DirContext context;
+        
+    // initialization ///////////////////////////////////////////////////////////////////////////
+    
+    /**
+     * Creates an instance of the user manager.
+     * 
+     * @param contextId the context identifier.
+     * @param namingPolicy the namig policy to be used.
+     * @param loginVerifier the login verifier.
+     * @param passwordGenerator the password generator.
+     * @param passwordDigester the password digester.
+     * @param factory the factory to get context from.
+     * @throws NamingException if the context could not be accessed.
+     */
+    public UserManager(String contextId, NamingPolicy namingPolicy, 
+        LoginVerifier loginVerifier, PasswordGenerator passwordGenerator, 
+        PasswordDigester passwordDigester, ContextFactory factory) 
+        throws NamingException
+    {
+        this.namingPolicy = namingPolicy;
+        this.loginVerifier = loginVerifier;
+        this.passwordGenerator = passwordGenerator;
+        this.passwordDigester = passwordDigester;
+        this.context = factory.getDirContext(contextId);
+    }
+    
+    // account creation + removal ///////////////////////////////////////////////////////////////
+    
+    /**
+     * Checks if a login name is a non-occupied and non-reserved one.
+     * 
+     * @param login the login name to be checked.
+     * @return <code>true</code> if a login name is a non-occupied and non-reserved.
+     * @throws AuthenticationException if there is a problem performing the operation.
+     */
+    public abstract boolean checkLogin(String login)
+       throws AuthenticationException;
+    
+    /**
+     * Creates a distinguished name from provided parameters in conformance to configured naming 
+     * policy.
+     * 
+     * @param parameters the parameters to generate name from.
+     * @return the distinghished name.
+     */
+    public String createDN(Parameters parameters)
+    {
+        return namingPolicy.getDn(parameters);
+    }
+    
+    /**
+     * Creates a new user account.
+     * 
+     * @param login login name of the user.
+     * @param dn distinguished name of the user.
+     * @param password initial password of the user.
+     * @return the newly created account.
+     * @throws AuthenticationException if the account could no be created.
+     */
+    public abstract Principal createAccount(String login, String dn, String password)
+        throws AuthenticationException;
+    
+    /**
+     * Removes an user account.
+     * 
+     * @param account the account.
+     * @throws AuthenticationException if the account could no be removed.
+     */
+    public abstract void removeAccount(Principal account)
+        throws AuthenticationException;
+
+    // user lookups /////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Lookup user by distinguised name.
+     *
+     * @param dn the users's distinguished name.
+     * @return the account's descriptor.
+     * @throws AuthenticationException if there is a problem performing the operation.
+     */
+    public abstract Principal getUserByName(String dn)
+        throws AuthenticationException;
+
+    /**
+     * Lookup user by login name.
+     *
+     * @param login the name used for authentication.
+     * @return the account's descriptor.
+     * @throws AuthenticationException if there is a problem performing the operation.
+     */
+    public abstract Principal getUserByLogin(String login)
+        throws AuthenticationException;
+
+    /**
+     * Maps user's distinguished name to login name.
+     *
+     * @param dn full user name.
+     * @return the login name, or <code>null</code> if not found.
+     * @throws AuthenticationException if there is a problem performing the operation.
+     * @throws InvalidNameException if the name does not conform to the configured naming policy.
+     */
+    public String getLogin(String dn)
+        throws AuthenticationException, InvalidNameException
+    {
+        return namingPolicy.getLogin(dn);
+    }
+
+    /**
+     * Returns the login name of an user.
+     *
+     * @param account the account.
+     * @return the login name, or <code>null</code> if not found.
+     * @throws AuthenticationException if there is a problem performing the operation.
+     * @throws InvalidNameException if the name does not conform to the configured naming policy.
+     */
+    public String getLogin(Principal account)
+        throws AuthenticationException, InvalidNameException
+    {
+        return namingPolicy.getLogin(account.getName());
+    }
+        
+    // system users /////////////////////////////////////////////////////////////////////////////
+    
+    /**
+     * Returns the anonymous account.
+     * 
+     * @return the anonyomous user.
+     * @throws AuthenticationException if there is a problem performing the operation.
+     */
+    public abstract Principal getAnonymousAccount()
+        throws AuthenticationException;
+
+    /**
+     * Returns the superuser account.
+     * 
+     * @return the superuser.
+     * @throws AuthenticationException if there is a problem performing the operation.
+     */
+    public abstract Principal getSuperuserAccount()
+        throws AuthenticationException;
+
+    // passwords ////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Changes user password.
+     * 
+     * @param account the account.
+     * @param password the new password for the account.
+     * @throws AuthenticationException if the password could not be changed.
+     */
+    public abstract void changeUserPassword(Principal account, String password)
+        throws AuthenticationException;
+        
+    /**
+     * Checks user supplied password.
+     * 
+     * @param account the account.
+     * @param password the password to be checked.
+     * @return <code>true</code> if the supplied password is correct.
+     * @throws AuthenticationException if there is a problem performing the operation.
+     */
+    public abstract boolean checkUserPassword(Principal account, String password)
+        throws AuthenticationException;
+        
+    /**
+     * Generates a random password.
+     * 
+     * @param min minimum length.
+     * @param max maximum length.
+     * @return a random passeword.
+     */
+    public String createRandomPassword(int min, int max)
+    {
+        return passwordGenerator.createRandomPassword(min, max);
+    }
+    
+    // personal data ////////////////////////////////////////////////////////////////////////////
+    
+    /**
+     * Returns the personal data of the accoun't owner.
+     * 
+     * @param account the account.
+     * @return Parameters view of the account's owner personal data. 
+     * @throws AuthenticationException if there is a problem performing the operation.
+     */
+    public abstract Parameters getPersonalData(Principal account)
+        throws AuthenticationException;
+        
+    /**
+     * Looks up user accounts according to personal data attributes.
+     * 
+     * @param attribute the personal data attribute name.
+     * @param value the personal data attribute value.
+     * @return the accounts that fulfill the condition.
+     * @throws NamingException if the opertion could not be performed.
+     */
+    public abstract Principal[] lookupAccounts(String attribute, String value)
+        throws NamingException;
+        
+    /**
+     * Looks up user accounts according to personal data attributes.
+     * 
+     * @param query the JNDI query in format supported by the underlying directory.
+     * @return the accounts that fulfill the condition.
+     * @throws NamingException if the opertion could not be performed.
+     */
+    public abstract Principal[] lookupAccounds(String query)
+        throws NamingException;    
+}
