8fc40834e150e74cd61e6564ca4c4e62fe014c53
Paweł Potempski <pawel.potempski@caltha.pl>
2004-02-20 10:01:02 +0000

    abstract classes replaced by default implementation

--- ledge-components/src/main/java/org/objectledge/authentication/PasswordDigester.java
+++ ledge-components/src/main/java/org/objectledge/authentication/PasswordDigester.java
@@ -1,45 +1,114 @@
 // 
-// Copyright (c) 2003, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
-// All rights reserved. 
+//Copyright (c) 2003, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+//All rights reserved. 
+//   
+//Redistribution and use in source and binary forms, with or without modification,  
+//are permitted provided that the following conditions are met: 
+//   
+//* Redistributions of source code must retain the above copyright notice,  
+//this list of conditions and the following disclaimer. 
+//* Redistributions in binary form must reproduce the above copyright notice,  
+//this list of conditions and the following disclaimer in the documentation  
+//and/or other materials provided with the distribution. 
+//* Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
+//nor the names of its contributors may be used to endorse or promote products  
+//derived from this software without specific prior written permission. 
 // 
-// Redistribution and use in source and binary forms, with or without modification,  
-// are permitted provided that the following conditions are met: 
-//  
-// * Redistributions of source code must retain the above copyright notice,  
-//	 this list of conditions and the following disclaimer. 
-// * Redistributions in binary form must reproduce the above copyright notice,  
-//	 this list of conditions and the following disclaimer in the documentation  
-//	 and/or other materials provided with the distribution. 
-// * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
-//	 nor the names of its contributors may be used to endorse or promote products  
-//	 derived from this software without specific prior written permission. 
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
-// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
-// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
-// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
-// POSSIBILITY OF SUCH DAMAGE. 
-// 
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
+//AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
+//WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+//IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
+//INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
+//BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+//OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+//WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
+//ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+//POSSIBILITY OF SUCH DAMAGE. 
+//
+
 package org.objectledge.authentication;
 
+import java.security.MessageDigest;
+
+import org.apache.commons.pool.BasePoolableObjectFactory;
+import org.apache.commons.pool.ObjectPool;
+import org.apache.commons.pool.impl.GenericObjectPool;
+
+import sun.misc.BASE64Encoder;
+
 /**
- * Digests passwords using hash algorithms.
+ * Default implementation of password digester.
  * 
- * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
- * @version $Id: PasswordDigester.java,v 1.1 2004-02-18 11:45:54 fil Exp $
+ * @author <a href="mailto:pablo@caltha.pl">Pawel Potempski</a>
  */
-public abstract class PasswordDigester
+public class PasswordDigester
 {
+    /** the digest algorithm - null for plaintext */
+    private String algorithm;
+
+    /** the local message digest pool */
+    private ObjectPool messageDigestPool = new GenericObjectPool(new MessageDigestFactory());
+
     /**
-     * Computes a message digest for a password.
+     * component constructor.
      * 
-     * @param password the password.
-     * @return the message digest of the password.
+     * @param algorithm the algorithm.
      */
-    public abstract String digestPassword(String password);
+    public PasswordDigester(String algorithm)
+    {
+        this.algorithm = algorithm;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String digestPassword(String password)
+    {
+        if (algorithm != null)
+        {
+            if (password == null)
+            {
+                // return unmatchable password for non-login accounts
+                return "-";
+            }
+            MessageDigest digest = null;
+            try
+            {
+                digest = (MessageDigest)messageDigestPool.borrowObject();
+                byte[] hash = digest.digest(password.getBytes());
+                messageDigestPool.returnObject(digest);
+                BASE64Encoder enc = new BASE64Encoder();
+                StringBuffer encoded = new StringBuffer();
+                encoded.append('{');
+                encoded.append(algorithm.toLowerCase());
+                encoded.append('}');
+                encoded.append(enc.encode(hash));
+                return encoded.toString();
+            }
+            catch (Exception e)
+            {
+                throw new IllegalArgumentException("Digest password exception: "+e.getMessage());
+            }
+        }
+        else
+        {
+            return password;
+        }
+    }
+    
+    /**
+     * A factory of MessageDigest objects.
+     */
+    private class MessageDigestFactory
+        extends BasePoolableObjectFactory
+    {
+        /**
+         * {@inheritDoc}
+         */
+        public Object makeObject() throws Exception
+        {
+            return MessageDigest.getInstance(algorithm);
+        }
+    }
+    
 }
--- ledge-components/src/main/java/org/objectledge/authentication/PasswordGenerator.java
+++ ledge-components/src/main/java/org/objectledge/authentication/PasswordGenerator.java
@@ -1,46 +1,87 @@
 // 
-// Copyright (c) 2003, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
-// All rights reserved. 
+//Copyright (c) 2003, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+//All rights reserved. 
+//   
+//Redistribution and use in source and binary forms, with or without modification,  
+//are permitted provided that the following conditions are met: 
+//   
+//* Redistributions of source code must retain the above copyright notice,  
+//this list of conditions and the following disclaimer. 
+//* Redistributions in binary form must reproduce the above copyright notice,  
+//this list of conditions and the following disclaimer in the documentation  
+//and/or other materials provided with the distribution. 
+//* Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
+//nor the names of its contributors may be used to endorse or promote products  
+//derived from this software without specific prior written permission. 
 // 
-// Redistribution and use in source and binary forms, with or without modification,  
-// are permitted provided that the following conditions are met: 
-//  
-// * Redistributions of source code must retain the above copyright notice,  
-//	 this list of conditions and the following disclaimer. 
-// * Redistributions in binary form must reproduce the above copyright notice,  
-//	 this list of conditions and the following disclaimer in the documentation  
-//	 and/or other materials provided with the distribution. 
-// * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
-//	 nor the names of its contributors may be used to endorse or promote products  
-//	 derived from this software without specific prior written permission. 
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
-// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
-// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
-// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
-// POSSIBILITY OF SUCH DAMAGE. 
-// 
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
+//AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
+//WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+//IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
+//INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
+//BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+//OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+//WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
+//ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+//POSSIBILITY OF SUCH DAMAGE. 
+//
+
 package org.objectledge.authentication;
 
+import java.util.Random;
+
 /**
- * Generates random passwords.
- *  
- * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
- * @version $Id: PasswordGenerator.java,v 1.1 2004-02-18 11:45:54 fil Exp $
+ * Default password generator.
+ * 
+ * @author <a href="mailto:pablo@caltha.pl">Pawel Potempski</a>
  */
-public abstract class PasswordGenerator
+public class PasswordGenerator 
 {
+    /** The pseudorandom numbers generator */
+    private Random randomGenerator;
+    
     /**
-     * Generates a random password.
-     * 
-     * @param min minimum length.
-     * @param max maximum length.
-     * @return a random passeword.
+     * Alphabet consisting of letters A-Z and digits 0-9.
      */
-    public abstract String createRandomPassword(int min, int max);    
+    private static final char[] ALPHABET = {
+        'A','B','C','D','E','F','G','H',
+        'I','J','K','L','M','N','O','P',
+        'Q','R','S','T','U','V','W','X',
+        'Y','Z','1','2','3','4','5','6',
+        '7','8','9','0','a','b','c','d',
+        'e','f','g','h','i','j','k','l',
+        'm','n','o','p','q','r','s','t',
+        'u','v','w','x','y','z',
+    };
+
+    /**
+     * Component constructor.
+     */
+    public PasswordGenerator()
+    {
+        randomGenerator = new Random();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String createRandomPassword(int min, int max)
+    {
+        int length = 0;
+        if (min < max)
+        {
+            int offset = randomGenerator.nextInt(max - min + 1);
+            length = min + offset;
+        }
+        else
+        {
+            length = min;
+        }
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++)
+        {
+            sb.append(ALPHABET[randomGenerator.nextInt(ALPHABET.length)]);
+        }
+        return sb.toString();
+    }
 }
