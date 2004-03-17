6b76b0e43e0895ad5c5a48d772643b904903dce2
Paweł Potempski <pawel.potempski@caltha.pl>
2004-03-17 12:54:39 +0000

    all test file fixed

--- ledge-components/src/test/java/org/objectledge/ComponentsTests.java
+++ ledge-components/src/test/java/org/objectledge/ComponentsTests.java
@@ -35,7 +35,7 @@ import junit.framework.TestSuite;
  *
  *
  * @author <a href="Rafal.Krzewski">rafal@caltha.pl</a>
- * @version $Id: ComponentsTests.java,v 1.21 2004-03-16 14:58:45 pablo Exp $
+ * @version $Id: ComponentsTests.java,v 1.22 2004-03-17 12:54:39 pablo Exp $
  */
 public class ComponentsTests
 {
@@ -52,7 +52,6 @@ public class ComponentsTests
         //$JUnit-BEGIN$
 
         //$JUnit-END$
-        /*
         suite.addTest(org.objectledge.authentication.AllTests.suite());
         suite.addTest(org.objectledge.cache.AllTests.suite());
         suite.addTest(org.objectledge.context.AllTests.suite());
@@ -65,15 +64,12 @@ public class ComponentsTests
         suite.addTest(org.objectledge.naming.AllTests.suite());
         suite.addTest(org.objectledge.parameters.AllTests.suite());
         suite.addTest(org.objectledge.pipeline.AllTests.suite());
-        */
         suite.addTest(org.objectledge.scheduler.AllTests.suite());
-        /*
         suite.addTest(org.objectledge.selector.AllTests.suite());
         suite.addTest(org.objectledge.templating.AllTests.suite());
         suite.addTest(org.objectledge.threads.AllTests.suite());
         suite.addTest(org.objectledge.utils.AllTests.suite());
         suite.addTest(org.objectledge.xml.AllTests.suite());
-        */
         return suite;
     }
 }
