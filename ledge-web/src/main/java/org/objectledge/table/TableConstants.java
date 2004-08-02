591c32bb7600e6d6ba36286e4d45a9bc04ba5b1e
Damian Gajda <damian.gajda@caltha.pl>
2004-08-02 14:35:48 +0000

    LEDGE-77 converted the names of parameters fot table toolkit

--- ledge-web/src/main/java/org/objectledge/table/TableConstants.java
+++ ledge-web/src/main/java/org/objectledge/table/TableConstants.java
@@ -33,22 +33,22 @@ package org.objectledge.table;
  *
  * @author <a href="mailto:dgajda@caltha.pl">Damian Gajda</a>
  * @author <a href="mailto:pablo@caltha.pl">Pawel Potempski</a>
- * @version $Id: TableConstants.java,v 1.2 2004-03-05 12:14:15 zwierzem Exp $
+ * @version $Id: TableConstants.java,v 1.3 2004-08-02 14:33:48 zwierzem Exp $
  */
 public interface TableConstants {
 
-    /** Table state id parameter key - <code>table_id</code>. */
-    public static final String TABLE_ID_PARAM_KEY = "table_id";
+    /** Table state id parameter key - <code>tableId</code>. */
+    public static final String TABLE_ID_PARAM_KEY = "tableId";
 
-    /** Row id parameter key - <code>row_id</code>. */
-    public static final String ROW_ID_PARAM_KEY = "row_id";
+    /** Row id parameter key - <code>rowId</code>. */
+    public static final String ROW_ID_PARAM_KEY = "rowId";
 
-    /** Page number parameter key - <code>page_nr</code>. */
+    /** Page number parameter key - <code>page</code>. */
     public static final String PAGE_NO_PARAM_KEY = "page";
 
-    /** Page number parameter key - <code>page_nr</code>. */
-    public static final String PAGE_SIZE_PARAM_KEY = "page_size";
+    /** Page number parameter key - <code>pageSize</code>. */
+    public static final String PAGE_SIZE_PARAM_KEY = "pageSize";
 
-    /** Sort column name parameter key - <code>sort_column</code>. */
-    public static final String SORT_COLUMN_PARAM_KEY = "sort_column";
+    /** Sort column name parameter key - <code>sortColumn</code>. */
+    public static final String SORT_COLUMN_PARAM_KEY = "sortColumn";
 }
