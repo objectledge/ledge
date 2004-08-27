a01750c3e01d09701f733c4ba4a066d660a2f327
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2004-08-27 07:57:51 +0000

    moved general purose actions from cyklotron to ledge-web

---
+++ ledge-web/src/main/java/org/objectledge/modules/actions/authentication/Logout.java
@@ -0,0 +1,84 @@
+// 
+// Copyright (c) 2003, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+// All rights reserved. 
+// 
+// Redistribution and use in source and binary forms, with or without modification,  
+// are permitted provided that the following conditions are met: 
+//  
+// * Redistributions of source code must retain the above copyright notice,  
+//	 this list of conditions and the following disclaimer. 
+// * Redistributions in binary form must reproduce the above copyright notice,  
+//	 this list of conditions and the following disclaimer in the documentation  
+//	 and/or other materials provided with the distribution. 
+// * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
+//	 nor the names of its contributors may be used to endorse or promote products  
+//	 derived from this software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+// POSSIBILITY OF SUCH DAMAGE. 
+// 
+package org.objectledge.modules.actions.authentication;
+
+import java.security.Principal;
+
+import org.jcontainer.dna.Logger;
+import org.objectledge.authentication.AuthenticationContext;
+import org.objectledge.authentication.AuthenticationException;
+import org.objectledge.authentication.UserManager;
+import org.objectledge.context.Context;
+import org.objectledge.pipeline.ProcessingException;
+import org.objectledge.pipeline.Valve;
+import org.objectledge.web.HttpContext;
+
+/**
+ * Logout action.
+ * 
+ * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
+ * @author <a href="mailto:pablo@caltha.pl">Pawel Potempski</a>
+ * @version $Id: Logout.java,v 1.1 2004-08-27 07:57:50 rafal Exp $ 
+ */
+public class Logout 
+    extends BaseAuthenticationAction
+    implements Valve
+{
+    /**
+     * Action constructor.
+     * 
+     * @param logger the logger.
+     * @param userManager the user manager.
+     */
+    public Logout(Logger logger, UserManager userManager)
+    {
+        super(logger, userManager);
+    }
+
+    /**
+     * Runns the valve.
+     *   
+     * @param context the context.
+     */
+    public void process(Context context) throws ProcessingException
+    {
+        try
+        {
+            HttpContext httpContext = HttpContext.getHttpContext(context);
+            clearSession(httpContext.getRequest().getSession());
+            Principal anonymous = userManager.getAnonymousAccount();
+            AuthenticationContext authenticationContext = 
+                AuthenticationContext.getAuthenticationContext(context);
+            authenticationContext.setUserPrincipal(anonymous, false);
+        }
+        catch(AuthenticationException e)
+        {
+            throw new ProcessingException("user manager exception ",e);
+        }
+    }
+}
