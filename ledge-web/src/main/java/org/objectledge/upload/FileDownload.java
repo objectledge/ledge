bcbad37853396ebd38504e165091b15705a3c309
Damian Gajda <damian.gajda@caltha.pl>
2004-10-04 14:25:04 +0000

    NUCYKLO-59 Added simple implementation of FileDownload functionality.

---
+++ ledge-web/src/main/java/org/objectledge/upload/FileDownload.java
@@ -0,0 +1,110 @@
+// 
+// Copyright (c) 2003, 2004, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+// All rights reserved. 
+//   
+// Redistribution and use in source and binary forms, with or without modification,  
+// are permitted provided that the following conditions are met: 
+//   
+// * Redistributions of source code must retain the above copyright notice,  
+//   this list of conditions and the following disclaimer. 
+// * Redistributions in binary form must reproduce the above copyright notice,  
+//   this list of conditions and the following disclaimer in the documentation  
+//   and/or other materials provided with the distribution. 
+// * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
+//   nor the names of its contributors may be used to endorse or promote products  
+//   derived from this software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+// POSSIBILITY OF SUCH DAMAGE. 
+//
+
+package org.objectledge.upload;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Date;
+
+import org.objectledge.context.Context;
+import org.objectledge.web.HttpContext;
+
+/**
+ * An utility for the file download functionality.
+ *
+ * @author <a href="dgajda@caltha.pl">Damian Gajda</a>
+ * @version $Id: FileDownload.java,v 1.1 2004-10-04 14:25:04 zwierzem Exp $
+ */
+public class FileDownload
+{
+    // instance variables ///////////////////////////////////////////////////////////////////////
+    
+    /** the thread's processing context. */
+    private Context context;
+
+    // initialization ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Creates a FileDownload component.
+     *
+     * @param config the configuration. 
+     * @param context the context.
+     */
+    public FileDownload(Context context)
+    {
+        this.context = context;
+    }
+    
+    // public API ///////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Dumps the <code>InputStream</code> contents as a direct response.
+     * 
+     * @param is the input stream of data to be downloaded
+     * @param contentType the content-type of downloaded data
+     * @param lastModified the last modification date of dowloaded data.
+     * @throws IOException thrown on errors while downloading
+     */
+    public void dumpData(InputStream is, String contentType, long lastModified)
+        throws IOException
+    {
+        HttpContext httpContext = HttpContext.getHttpContext(context);
+        httpContext.setContentType(contentType);
+        httpContext.getResponse().addDateHeader("Last-Modified", lastModified);
+        OutputStream os = httpContext.getOutputStream();
+        byte[] buffer = new byte[is.available() > 0 ? is.available() : 4096];
+        int count = 0;
+        while(count >= 0)
+        {
+            count = is.read(buffer,0,buffer.length);
+            if(count > 0)
+            {
+                os.write(buffer, 0, count);
+            }
+        }
+        is.close();
+        os.flush();
+    }
+
+    /**
+     * Dumps the <code>InputStream</code> contents as a direct response with current time as last
+     * modification time
+     * 
+     * @param is the input stream of data to be downloaded
+     * @param contentType the content-type of downloaded data
+     * @throws IOException thrown on errors while downloading
+     */
+    public void dumpData(InputStream is, String contentType)
+        throws IOException
+    {
+        Date date = new Date();
+        dumpData(is, contentType, date.getTime());
+    }
+}
