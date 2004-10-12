a8f6b200445533f68d1c1705999b5ca6ea5103cb
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2004-10-12 13:18:04 +0000

    plugging in damagecontrol

--- CVSROOT/logcheck.sh
+++ CVSROOT/logcheck.sh
@@ -17,6 +17,11 @@ $CVSROOT/CVSROOT/log_accum.pl $USER "`echo ${1} | cut -d , -f 1`" < $TEMPFILE
 $CVSROOT/CVSROOT/collect_diffs.rb --from $USER --to objectledge-cvshtml@lists.caltha.pl "${1}" < $TEMPFILE
 
 #
+# Invoke DamageControl
+#
+/home/damagecontrol/current/bin/requestbuild --url http://localhost:4712/private/xmlrpc --projectname `echo ${1} | cut -d/ -f1`
+
+#
 # clean up
 #
 rm $TEMPFILE
