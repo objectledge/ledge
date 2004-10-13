46456413d755fb4a0c3d4128a167f473a7b3b5d0
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2004-10-13 06:03:00 +0000

    tweaking DC

--- CVSROOT/logcheck.sh
+++ CVSROOT/logcheck.sh
@@ -19,7 +19,7 @@ $CVSROOT/CVSROOT/collect_diffs.rb --from $USER --to objectledge-cvshtml@lists.ca
 #
 # Invoke DamageControl
 #
-/home/damagecontrol/current/bin/requestbuild --url http://localhost:4712/private/xmlrpc --projectname `echo ${1} | cut -d/ -f1`
+ruby /home/damagecontrol/current/bin/requestbuild --url http://localhost:4712/private/xmlrpc --projectname `echo ${1} | cut -d/ -f1`
 
 #
 # clean up
