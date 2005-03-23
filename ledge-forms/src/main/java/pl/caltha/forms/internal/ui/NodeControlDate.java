c6bac43a017e30dcc11e48374a7571c3bcfacdb5
Damian Gajda <damian.gajda@caltha.pl>
2005-03-23 05:36:31 +0000

    Updated date control to new features

--- ledge-forms/src/main/java/pl/caltha/forms/internal/ui/NodeControlDate.java
+++ ledge-forms/src/main/java/pl/caltha/forms/internal/ui/NodeControlDate.java
@@ -13,7 +13,7 @@ import pl.caltha.forms.internal.util.Util;
  * Implementation of Date controls.
  *
  * @author <a href="mailto:dgajda@caltha.pl">Damian Gajda</a>
- * @version $Id: NodeControlDate.java,v 1.1 2005-01-19 06:55:28 pablo Exp $
+ * @version $Id: NodeControlDate.java,v 1.2 2005-03-23 05:36:31 zwierzem Exp $
  */
 public class NodeControlDate extends NodeControl
 {
@@ -22,7 +22,7 @@ public class NodeControlDate extends NodeControl
     {
         super(type, atts);
 
-		optionalSelection = Util.createBooleanAttribute(atts, "optionalSelection", false);
+        optionalSelection = Util.createBooleanAttribute(atts, "optionalSelection", false);
     }
 
     //------------------------------------------------------------------------
@@ -36,51 +36,91 @@ public class NodeControlDate extends NodeControl
         return optionalSelection;
     }
 
-    /* (non-Javadoc)
-     * @see pl.caltha.forms.internal.ui.Control#getValue(pl.caltha.forms.internal.model.InstanceImpl)
+    /** This method breakes the contract for returning <code>null</code> for undefined value.
+     *  That is why the hasValue method had to be reimplemented.
      */
     public Object getValue(InstanceImpl instance)
     {
-		Object value = super.getValue(instance);
-		if(value != null && value instanceof String && ((String)value).length() > 0)
-		{
-            if(value.equals("now"))
+        Object value = super.getValue(instance);
+        if(value != null && value instanceof String && ((String)value).length() > 0)
+        {
+            String stringValue = (String) value;
+            Date dateValue = null;
+            
+            if(stringValue.startsWith("now"))
             {
-                Date date = new Date(); 
-                return new Long(date.getTime());
+                dateValue = new Date();
             }
             else
-            if(value.equals("today-start"))
+            if(stringValue.startsWith("today-start"))
             {
                 Calendar calendar = Calendar.getInstance();
                 calendar.set(Calendar.HOUR_OF_DAY, 0);
                 calendar.set(Calendar.MINUTE, 0);
                 calendar.set(Calendar.SECOND, 0);
                 calendar.set(Calendar.MILLISECOND, 0);
-                return new Long(calendar.getTimeInMillis());
+                dateValue = calendar.getTime();
             }
             else
-            if(value.equals("today-end"))
+            if(stringValue.startsWith("today-end"))
             {
                 Calendar calendar = Calendar.getInstance();
                 calendar.set(Calendar.HOUR_OF_DAY, 23);
                 calendar.set(Calendar.MINUTE, 59);
                 calendar.set(Calendar.SECOND, 59);
                 calendar.set(Calendar.MILLISECOND, 999);
-                return new Long(calendar.getTimeInMillis());
+                dateValue = calendar.getTime();
             }
             else
             {
-    			try
-    			{
-    				return new Long((String)value);
-    			}
-    			catch(NumberFormatException e)
-    			{
-    				return null;
-    			}
+                try
+                {
+                    int end = stringValue.indexOf('/');
+                    end = (end == -1) ? stringValue.length() : end; 
+                    dateValue = new Date(Long.parseLong(stringValue.substring(0, end)));
+                }
+                catch(NumberFormatException e)
+                {
+                    dateValue = null; // value undefined
+                }
             }
-		}
-		return null;
+
+            return (optionalSelection)
+                ? new DateValue(dateValue, !stringValue.endsWith("/disabled"))
+                : new DateValue(dateValue);
+        }
+        return new DateValue(null);
+    }
+    
+    public boolean hasValue(InstanceImpl instance)
+    {
+        Object value = getValue(instance);
+        return ((DateValue)value).getValue() != null ;
+    }
+    
+    public class DateValue
+    {
+        private Date value;
+        private boolean enabled;
+        
+        public DateValue(Date value)
+        {
+            this(value, true);
+        }
+
+        public DateValue(Date value, boolean enabled)
+        {
+            this.value = value;
+            this.enabled = enabled;
+        }
+
+        public boolean getEnabled()
+        {
+            return enabled;
+        }
+        public Date getValue()
+        {
+            return value;
+        }
     }
 }
