5ccea8c3d6ad468c229ad981a1ed25af6654c60b
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2005-07-25 12:59:09 +0000

    javadocs
    submitted by Damian

--- ledge-components/src/main/java/org/objectledge/hibernate/HibernateSessionContext.java
+++ ledge-components/src/main/java/org/objectledge/hibernate/HibernateSessionContext.java
@@ -4,10 +4,38 @@ import org.hibernate.Session;
 import org.objectledge.context.Context;
 
 /**
- * The hibernate session context.
+ * The hibernate session context is used to access the hiberate session for current request.
+ *
+ * <p>
+ * Example code:
+ * </p>
+ * <pre>
+ * import org.hibernate.Session;
+ * import org.hibernate.Transaction;
+ * ...
+ * import org.objectledge.hibernate.HibernateSessionContext;
+ * import org.objectledge.hibernate.HibernateSessionFactory;
+ * 
+ * ...
+ * 
+ * Session x = HibernateSessionContext.getHibernateSessionContext(context).getSession();
+ * Transaction tx = x.beginTransaction();
+ * try
+ * {
+ *     Event e = new Event();
+ *     e.setTitle("costam");
+ *     x.save(e);
+ *     tx.commit();
+ *     templatingContext.put("events", x.createQuery("from Event").list());
+ * }
+ * catch (Exception e)
+ * {
+ *     tx.rollback();
+ * }
+ * </pre>
  *
  * @author <a href="mailto:mgolebsk@elka.pw.edu.pl">Marcin Golebski</a>
- * @version $Id: HibernateSessionContext.java,v 1.1 2005-07-22 17:19:39 pablo Exp $
+ * @version $Id: HibernateSessionContext.java,v 1.2 2005-07-25 12:59:09 rafal Exp $
  */
 public class HibernateSessionContext
 {
