da915d17b99bad9b8cea2d765b07e73ab92d1820
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2005-07-26 12:10:26 +0000

    separated DefaultComponent class into AbstractComponent class ment for extension and final DefaultComponent class that simply renders the template located by finder. added process(TemplatingContext tc) method intended for extension instead of build(..) method in the old setup.

--- ledge-web/src/main/java/org/objectledge/web/mvc/components/AbstractComponent.java
+++ ledge-web/src/main/java/org/objectledge/web/mvc/components/AbstractComponent.java
@@ -28,21 +28,70 @@
 package org.objectledge.web.mvc.components;
 
 import org.objectledge.context.Context;
+import org.objectledge.pipeline.ProcessingException;
+import org.objectledge.templating.MergingException;
+import org.objectledge.templating.Template;
+import org.objectledge.templating.TemplatingContext;
+import org.objectledge.web.mvc.builders.BuildException;
 
 /**
- * Default component implementation that only renders the template.
+ * Base class of Component interface implementations.
  * 
- * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
- * @version $Id: DefaultComponent.java,v 1.6 2005-07-26 12:10:26 rafal Exp $
+ * @author <a href="mailto:dgajda@caltha.pl">Damian Gajda</a>
+ * @version $Id: AbstractComponent.java,v 1.1 2005-07-26 12:10:26 rafal Exp $
  */
-public final class DefaultComponent
-    extends AbstractComponent
+public abstract class AbstractComponent 
+    implements Component
 {
+	/** Application context used by this component instance. */
+	protected Context context;
+	
+	/**
+	 * Constructs a component instance.
+	 * 
+	 * @param context application context for use by this component.
+	 */
+	public AbstractComponent(Context context)
+	{
+		this.context = context;
+	}	
+
     /**
-     * @param context the request context.
+     * Perform processing and store values in the tmplating context.
+     * 
+     * @param templatingContext the TemplatingContext.
+     * @throws ProcessingException if the processing fails.
      */
-    public DefaultComponent(Context context)
+    public void process(TemplatingContext templatingContext)
+        throws ProcessingException
     {
-        super(context);
+        // do nothing in base implementation
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String build(Template template)
+    	throws BuildException, ProcessingException
+    {
+        TemplatingContext templatingContext = TemplatingContext.getTemplatingContext(context);
+        process(templatingContext);
+		try
+		{
+			return template.merge(templatingContext);
+		}
+		catch(MergingException e)
+		{
+			throw new BuildException("failed to merge template: "+template.getName(), e);
+		}
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Template getTemplate()
+        throws ProcessingException
+    {
+        return null;
     }
 }
