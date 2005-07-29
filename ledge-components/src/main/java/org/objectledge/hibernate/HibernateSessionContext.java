fbe0bb07a43528b9fddb303d6d1b7cc66a8525e3
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2005-07-29 14:39:52 +0000

    license

--- ledge-components/src/main/java/org/objectledge/hibernate/HibernateSessionContext.java
+++ ledge-components/src/main/java/org/objectledge/hibernate/HibernateSessionContext.java
@@ -1,3 +1,30 @@
+// 
+// Copyright (c) 2003-2005, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+// All rights reserved. 
+//   
+// Redistribution and use in source and binary forms, with or without modification,  
+// are permitted provided that the following conditions are met: 
+//   
+// * Redistributions of source code must retain the above copyright notice,  
+// this list of conditions and the following disclaimer. 
+// * Redistributions in binary form must reproduce the above copyright notice,  
+// this list of conditions and the following disclaimer in the documentation  
+// and/or other materials provided with the distribution. 
+// * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
+// nor the names of its contributors may be used to endorse or promote products  
+// derived from this software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+// POSSIBILITY OF SUCH DAMAGE. 
+//
 package org.objectledge.hibernate;
 
 import org.hibernate.Session;
@@ -35,7 +62,7 @@ import org.objectledge.context.Context;
  * </pre>
  *
  * @author <a href="mailto:mgolebsk@elka.pw.edu.pl">Marcin Golebski</a>
- * @version $Id: HibernateSessionContext.java,v 1.2 2005-07-25 12:59:09 rafal Exp $
+ * @version $Id: HibernateSessionContext.java,v 1.3 2005-07-29 14:39:05 rafal Exp $
  */
 public class HibernateSessionContext
 {
