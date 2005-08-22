6132a114a975684f8987751a3f8eb5a881f19788
Damian Gajda <damian.gajda@caltha.pl>
2005-08-22 14:13:37 +0000

    Split up the parameters tool to allow use of parmaters tool for any kind of parameters set.

--- ledge-web/src/main/java/org/objectledge/web/mvc/tools/ParametersTool.java
+++ ledge-web/src/main/java/org/objectledge/web/mvc/tools/ParametersTool.java
@@ -28,11 +28,6 @@
 
 package org.objectledge.web.mvc.tools;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-
 import org.objectledge.parameters.RequestParameters;
 import org.objectledge.web.mvc.MVCContext;
 
@@ -40,12 +35,12 @@ import org.objectledge.web.mvc.MVCContext;
  * Give a read only access to request parameters including MVC parameters.
  * 
  * @author <a href="mailto:dgajda@caltha.pl">Damian Gajda</a>
- * @version $Id: ParametersTool.java,v 1.7 2005-08-05 12:48:24 rafal Exp $
+ * @version $Id: ParametersTool.java,v 1.8 2005-08-22 14:13:37 zwierzem Exp $
  */
 public class ParametersTool
+extends AnyParametersTool
 {
     private MVCContext mvcContext;
-    private RequestParameters parameters;
 
     /**
      * Creates the parameters tool for a given set of parameters and mvc context.
@@ -55,8 +50,8 @@ public class ParametersTool
      */
     public ParametersTool(MVCContext mvcContext, RequestParameters parameters)
     {
+       super(parameters);
        this.mvcContext = mvcContext;
-       this.parameters = parameters; 
     }
 
     /**
@@ -80,255 +75,6 @@ public class ParametersTool
     }
     
     /**
-     * Return the parameter with specified name. 
-     * 
-     * @param name the name of the parameter.
-     * @return the string value of the parameter.
-     */
-    public String get(String name)
-    {
-        return parameters.get(name);
-    }
-
-    /**
-     * Return the parameter with specified name. 
-     * 
-     * @param name the name of the parameter.
-     * @param defaultValue the default value of the parameter.
-     * @return the string value of the parameter.
-     */
-    public String get(String name, String defaultValue)
-    {
-        return parameters.get(name, defaultValue);
-    }
-
-    /**
-     * Return all values of the parameter with specified name as an array. 
-     * 
-     * @param name the name of the parameters.
-     * @return the array of the string values of the parameter.
-    */
-    public String[] getStrings(String name)
-    {
-        return parameters.getStrings(name);
-    }
-
-    /**
-     * Return all values of the parameter with specified name as a list of strings. 
-     * 
-     * @param name the name of the parameters.
-     * @return the list of the string values of the parameter.
-    */
-    public List<String> getStringsList(String name)
-    {
-        String[] a = parameters.getStrings(name);
-        List<String> l = new ArrayList(a.length);
-        l.addAll(Arrays.asList(a));
-        return l;
-    }
-
-    /**
-     * Return the parameter with specified name. 
-     * 
-     * @param name the name of the parameter.
-     * @return the boolean value of the parameter.
-     */
-    public boolean getBoolean(String name)
-    {
-        return parameters.getBoolean(name);
-    }
-
-    /**
-     * Return the parameter with specified name. 
-     * 
-     * @param name the name of the parameter.
-     * @param defaultValue the default value of the parameter.
-     * @return the boolean value of the parameter.
-     */
-    public boolean getBoolean(String name, boolean defaultValue)
-    {
-        return parameters.getBoolean(name, defaultValue);
-    }
-
-    /**
-     * Return all values of the parameter with specified name as an array. 
-     * 
-     * @param name the name of the parameters.
-     * @return the array of the boolean values of the parameter.
-    */
-    public boolean[] getBooleans(String name)
-    {
-        return parameters.getBooleans(name);
-    }
-
-    /**
-     * Return the parameter with specified name.
-     * The assumed String value of this parameter is a decimal representation of a Unix time-stamp.
-     * 
-     * @param name the name of the parameter.
-     * @return the date value of the parameter.
-     */
-    public Date getDate(String name)
-    {
-        return parameters.getDate(name);
-    }
-
-    /**
-     * Return the parameter with specified name. 
-     * The assumed String value of this parameter is a decimal representation of a Unix time-stamp.
-     * 
-     * @param name the name of the parameter.
-     * @param defaultValue the default value of the parameter.
-     * @return the date value of the parameter.
-     */
-    public Date getDate(String name, Date defaultValue)
-    {
-        return parameters.getDate(name, defaultValue);
-    }
-
-    /**
-     * Return all values of the parameter with specified name as an array. 
-     * The assumed String values of this parameter is are decimal representations of Unix
-     * time-stamps.
-     * 
-     * @param name the name of the parameters.
-     * @return the array of the date values of the parameter.
-    */
-    public Date[] getDates(String name)
-    {
-        return parameters.getDates(name);
-    }
-
-    /**
-     * Return the parameter with specified name. 
-     * 
-     * @param name the name of the parameter.
-     * @return the float value of the parameter.
-     * @throws NumberFormatException if parameter is not a number.
-     */
-    public float getFloat(String name) throws NumberFormatException
-    {
-        return parameters.getFloat(name);
-    }
-
-    /**
-     * Return the parameter with specified name. 
-     * 
-     * @param name the name of the parameter.
-     * @param defaultValue the default value of the parameter.
-     * @return the float value of the parameter.
-     */
-    public float getFloat(String name, float defaultValue)
-    {
-        return parameters.getFloat(name, defaultValue);
-    }
-
-    /**
-     * Return all values of the parameter with specified name as an array. 
-     * 
-     * @param name the name of the parameters.
-     * @return the array of the float values of the parameter.
-     * @throws NumberFormatException if anyone of the values is not a number. 
-    */
-    public float[] getFloats(String name) throws NumberFormatException
-    {
-        return parameters.getFloats(name);
-    }
-
-    /**
-     * Return the parameter with specified name. 
-     * 
-     * @param name the name of the parameter.
-     * @return the integer value of the parameter.
-     * @throws NumberFormatException if parameter is not a number.
-     */
-    public int getInt(String name) throws NumberFormatException
-    {
-        return parameters.getInt(name);
-    }
-
-    /**
-     * Return the parameter with specified name. 
-     * 
-     * @param name the name of the parameter.
-     * @param defaultValue the default value of the parameter.
-     * @return the integer value of the parameter.
-     */
-    public int getInt(String name, int defaultValue)
-    {
-        return parameters.getInt(name, defaultValue);
-    }
-
-    /**
-     * Return all values of the parameter with specified name as an array. 
-     * 
-     * @param name the name of the parameters.
-     * @return the array of the integer values of the parameter.
-     * @throws NumberFormatException if anyone of the values is not a number.
-    */
-    public int[] getInts(String name) throws NumberFormatException
-    {
-        return parameters.getInts(name);
-    }
-
-    /**
-     * Return the parameter with specified name. 
-     * 
-     * @param name the name of the parameter.
-     * @return the integer value of the parameter.
-     * @throws NumberFormatException if parameter is not a number.
-     */
-    public long getLong(String name) throws NumberFormatException
-    {
-        return parameters.getLong(name);
-    }
-
-    /**
-     * Return the parameter as array of long values. 
-     * 
-     * @param name the name of the parameter.
-     * @return the array of parameter values.
-     * @throws NumberFormatException if parameter is not a number.
-     */
-    public long[] getLongs(String name) throws NumberFormatException
-    {
-        return parameters.getLongs(name);
-    }
-
-    /**
-     * Return the parameter with specified name. 
-     * 
-     * @param name the name of the parameter.
-     * @param defaultValue the default value of the parameter.
-     * @return the integer value of the parameter.
-     */
-    public long getLong(String name, long defaultValue)
-    {
-        return parameters.getLong(name, defaultValue);
-    }
-
-    /**
-     * Return the names of all parameters.
-     * 
-     * @return the parameter names.
-     */
-    public List<String> getParameterNames()
-    {
-        return Arrays.asList(parameters.getParameterNames());
-    }
-
-    /**
-     * Checks whether parameter is defined.
-     * 
-     * @param name the name of the parameter.
-     * @return <code>true</code> if parameter is defined.
-     */
-    public boolean isDefined(String name)
-    {
-        return parameters.isDefined(name);
-    }
-    
-    /**
      * Checks if the parameter was passed in through request path info.
      * 
      * @param name name of the parameter.
@@ -336,7 +82,7 @@ public class ParametersTool
      */
     public boolean isPathInfoParameter(String name)
     {
-        return parameters.isPathInfoParameter(name);
+        return ((RequestParameters)parameters).isPathInfoParameter(name);
     }
 
     /**
@@ -347,7 +93,7 @@ public class ParametersTool
      */
     public boolean isQueryStringParameter(String name)
     {
-        return parameters.isQueryStringParameter(name);
+        return ((RequestParameters)parameters).isQueryStringParameter(name);
     }
     
     /**
@@ -358,6 +104,6 @@ public class ParametersTool
      */
     public boolean isPOSTParameter(String name)
     {
-        return parameters.isPOSTParameter(name);
+        return ((RequestParameters)parameters).isPOSTParameter(name);
     }    
 }
