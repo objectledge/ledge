b7b5bf988fd119ed86c6df8e9845f7ee567e3d0a
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2007-11-18 21:05:45 +0000

    fixed some deprecation errors

--- ledge-components/src/main/java/org/objectledge/templating/velocity/VelocityTemplating.java
+++ ledge-components/src/main/java/org/objectledge/templating/velocity/VelocityTemplating.java
@@ -54,7 +54,7 @@ import org.objectledge.templating.TemplatingContext;
  *
  *
  * @author <a href="mailto:pablo@caltha.com">Pawel Potempski</a>
- * @version $Id: VelocityTemplating.java,v 1.25 2006-03-10 08:17:32 zwierzem Exp $
+ * @version $Id: VelocityTemplating.java,v 1.26 2007-11-18 21:05:45 rafal Exp $
  */
 public class VelocityTemplating implements Templating, LogSystem
 {
@@ -207,7 +207,7 @@ public class VelocityTemplating implements Templating, LogSystem
             for (int i = 0; i < paths.length; i++)
             {
                 String path = paths[i] + name + extension;
-                if (engine.templateExists(path))
+                if (engine.resourceExists(path))
                 {
                     exists = true;
                     break;
@@ -265,7 +265,7 @@ public class VelocityTemplating implements Templating, LogSystem
             for (int i = 0; i < paths.length; i++)
             {
                 path = paths[i] + name + extension;
-                if (engine.templateExists(path))
+                if (engine.resourceExists(path))
                 {
                     template = new VelocityTemplate(this, name, engine.getTemplate(path));
                 }
