e2bd39fdad22261ab8884134f6c7c6c8ee1ac3e8
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2008-01-02 22:29:59 +0000

    Much simpler and also more effective implementation using java.util.LinkedHashMap JDK is a like treasure chest...

--- ledge-components/src/main/java/org/objectledge/cache/impl/LRUMapImpl.java
+++ ledge-components/src/main/java/org/objectledge/cache/impl/LRUMapImpl.java
@@ -1,321 +1,144 @@
-//
-//Copyright (c) 2003, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.
-//All rights reserved.
-//
-//Redistribution and use in source and binary forms, with or without modification, 
-//are permitted provided that the following conditions are met:
-//
-//* Redistributions of source code must retain the above copyright notice, 
-//this list of conditions and the following disclaimer.
-//* Redistributions in binary form must reproduce the above copyright notice, 
-//this list of conditions and the following disclaimer in the documentation 
-//and/or other materials provided with the distribution.
-//* Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
-//nor the names of its contributors may be used to endorse or promote products 
-//derived from this software without specific prior written permission.
-//
-//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-//AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-//WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-//IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-//INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
-//BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
-//OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-//WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-//ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
-//POSSIBILITY OF SUCH DAMAGE.
-//
-
 package org.objectledge.cache.impl;
 
-import java.util.AbstractCollection;
-import java.util.AbstractSet;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Set;
+import java.util.LinkedHashMap;
 
 import org.objectledge.cache.spi.CacheFactorySPI;
 import org.objectledge.cache.spi.ConfigurableMap;
 import org.objectledge.cache.spi.LRUMap;
 
 /**
- * An implementation of soft cache.
- *
- * <p>This implementation performs LRU ordering on each get() operation
- * with o(n) complexity. This approach is slower on get-intensive maps,
- * but is faster on put-intensive maps. This map is supposed to be used as a
- * limited space storage below a factory map, so there is a good chance that
- * it will be put-intensive. Moreover, this approach is by far simpler to
- * implement than put() LRU ordering ;-)</p>
+ * An implementation of LRUMap interface using java.util.LinkedHashMap
  * 
- * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
- * @version $Id: LRUMapImpl.java,v 1.6 2007-06-28 20:56:56 rafal Exp $
+ * @author Rafał Krzewski
  */
 public class LRUMapImpl
     extends DelegateMap
     implements LRUMap, ConfigurableMap
 {
-    // constatns ////////////////////////////////////////////////////////////
+    /** Default capacity o the map (1000) */
+    private static final int CAPACITY_DEFAULT = 1000;
 
-    /** The default map capacity. (10) */
-    public static final int CAPACITY_DEFAULT = 10;
+    /** Default load factor of the map (0.75) */
+    private static final float LOAD_FACTOR_DEFAULT = 0.75f;
 
-    // instance variables ///////////////////////////////////////////////////
-
-    /** The capacity limit. */
-    private int capacity;
-
-    /** The access list. */
-    private LinkedList<Object> access = new LinkedList<Object>();
-
-    // initailization ////////////////////////////////////////////////////////
+    /** Strongly typed delegate method reference, for setCapacity()/getCapacity() access */
+    private Storage map;
 
     /**
-     * Constructs a soft chache.
+     * Creates an instance of the map with the default parameters.
      */
     public LRUMapImpl()
     {
-        super(new HashMap(CAPACITY_DEFAULT));
+        super(new Storage(CAPACITY_DEFAULT, LOAD_FACTOR_DEFAULT));
+        map = (Storage)getDelegate();
     }
 
-    // ConfigurableMap interface /////////////////////////////////////////////
+    // ConfigurableMap interface /////////////////////////////////////////////////////////////////
 
     /**
-     * {@inheritDoc}
+     * Configures the map.
+     * <p>
+     * Two configuration options are supported
+     * <ul>
+     * <li><i>capacity</i></li>
+     * <li><i>capacity</i>,<i>loadFactor</i></li>
+     * </ul>
+     * Where <i>capacity</i> is a positive integer, and loadFactor is a floating point number from
+     * the range (0,1)
+     * </p>
      */
     public void configure(CacheFactorySPI caching, String name, String config)
     {
         try
         {
-            setCapacity(Integer.parseInt(config));
+            if(config.contains(","))
+            {
+                String[] c = config.split(",");
+                if(c.length != 2)
+                {
+                    throw new IllegalArgumentException("invalid spec \"" + config
+                        + "\" expected size or size,loadFactor");
+                }
+                int capacity = Integer.parseInt(c[0]);
+                float loadFactor = Float.parseFloat(c[1]);
+                setDelegate(new Storage(capacity, loadFactor));
+                map = (Storage)getDelegate();
+            }
+            else
+            {
+                setCapacity(Integer.parseInt(config));
+            }
         }
         catch(NumberFormatException e)
         {
-            throw new IllegalArgumentException("invalid capacity spec '"+config+"' for '"+name+"'");
+            throw new IllegalArgumentException("invalid capacity spec '" + config + "' for '"
+                + name + "'");
         }
     }
 
-    // LRUMap SPI interface //////////////////////////////////////////////////
+    // LRUMap SPI interface //////////////////////////////////////////////////////////////////////
+    
+    /**
+     * {@inheritDoc}
+     */
+    public int getCapacity()
+    {
+        return map.getCapacity();
+    }
 
     /**
      * {@inheritDoc}
      */
     public void setCapacity(int capacity)
     {
-        if(capacity < access.size())
-        {
-            access = new LinkedList<Object>(access.subList(0, capacity-1));
-        }
-        this.capacity = capacity;
+        map.setCapacity(capacity);
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    public int getCapacity()
-    {
-        return capacity;
-    }
-
-    // Map interface /////////////////////////////////////////////////////////
-
-    /**
-     * {@inheritDoc}
-     */
-    public Object put(Object key, Object value)
-    {
-        while(delegate.size() >= capacity)
-        {
-            delegate.remove(access.removeLast());
-        }
-        access.remove(key);
-        access.addFirst(key);
-        return delegate.put(key, value);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public Object get(Object key)
-    {
-        Object obj = delegate.get(key);
-        if(obj != null)
-        {
-            access.remove(key);
-            access.addFirst(key);
-        }
-        return obj;
-    }
+    //////////////////////////////////////////////////////////////////////////////////////////////
     
-    public boolean containsKey(Object key)
+    private static class Storage
+        extends LinkedHashMap<Object, Object>
     {
-        boolean contains = delegate.containsKey(key);
-        if(contains)
+        /** Map capacity */
+        private int capacity;
+
+        /**
+         * Creates Storage instance
+         * 
+         * @param capacity map capacity.
+         * @param loadFactor map load factor.
+         */
+        public Storage(int capacity, float loadFactor)
         {
-            access.remove(key);
-            access.addFirst(key);            
+            // Create the superclass with accessOrder = true
+            super(capacity, loadFactor, true);
+            this.capacity = capacity;
         }
-        return contains;
-    }
 
-    /**
-     * {@inheritDoc}
-     */
-    public Object remove(Object key)
-    {
-        Object old = delegate.remove(key);
-        access.remove(key);
-        return old;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public Set entrySet()
-    {
-        final Set entries = delegate.entrySet();
-        
-        return new AbstractSet()
-            {
-                public Iterator iterator()
-                {
-                    return getEntryIterator();
-                }
-                
-                public boolean contains(Object o)
-                {
-                    if(o instanceof Map.Entry)
-                    {
-                        Object key = ((Map.Entry)o).getKey();
-                        access.remove(key);
-                        access.addFirst(key);
-                    }
-                    return entries.contains(o);
-                }
-                
-                public boolean remove(Object o)
-                {
-                    if(o instanceof Map.Entry)
-                    {
-                        Object key = ((Map.Entry)o).getKey();
-                        access.remove(key);
-                    }
-                    return entries.remove(o);
-                }
-                
-                public int size()
-                {
-                    return delegate.size();
-                }
-                
-                public void clear()
-                {
-                    delegate.clear();
-                    access.clear();
-                }
-            };
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public Collection values()
-    {
-        return new AbstractCollection()
-            {
-                public Iterator iterator()
-                {
-                    return getValueIterator();
-                }
-                
-                public boolean contains(Object value)
-                {
-                    return containsValue(value);
-                }
-                
-                public int size()
-                {
-                    return delegate.size();
-                }
-                
-                public void clear()
-                {
-                    delegate.clear();
-                    access.clear();
-                }
-            };
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    Iterator getValueIterator()
-    {
-        if(delegate.isEmpty())
+        @Override
+        protected boolean removeEldestEntry(Entry<Object, Object> eldest)
         {
-            return new EmptyIterator();
+            return size() >= capacity;
         }
-        else
-        {
-            final Iterator i = entrySet().iterator();
-            return new Iterator()
-                {
-                    public boolean hasNext()
-                    {
-                        return i.hasNext();
-                    }
-                    
-                    public Object next()
-                    {
-                        return ((Map.Entry)i.next()).getValue();
-                    }
-                    
-                    public void remove()
-                    {
-                        i.remove();
-                    }
-                };
-        }
-    }
 
-    /**
-     * {@inheritDoc}
-     */
-    Iterator getEntryIterator()
-    {
-        if(delegate.isEmpty())
+        /**
+         * Returns current map capacity.
+         * 
+         * @return map capacity.
+         */
+        public int getCapacity()
         {
-            return new EmptyIterator();
+            return capacity;
         }
-        else
+
+        /**
+         * Sets new map capacity.
+         * 
+         * @param capacity map capacity.
+         */
+        public void setCapacity(int capacity)
         {
-            final Iterator i = delegate.entrySet().iterator();
-            return new Iterator()
-                {
-                    private Object lastKey = null;
-                    public boolean hasNext()
-                    {
-                        return i.hasNext();
-                    }
-                    
-                    public Object next()
-                    {
-                        Map.Entry mapEntry = (Map.Entry)i.next();
-                        lastKey = mapEntry.getKey();
-                        access.remove(lastKey);
-                        access.addFirst(lastKey);
-                        return mapEntry;
-                    }
-                    
-                    public void remove()
-                    {
-                        i.remove();
-                        access.remove(lastKey);
-                    }
-                };
+            this.capacity = capacity;
         }
     }
 }
