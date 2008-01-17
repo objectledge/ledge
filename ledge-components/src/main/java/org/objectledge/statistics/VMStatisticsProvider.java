48b0dcd2b3d8b62b6d5d3a01ae81f42223344471
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2008-01-17 22:56:55 +0000

    CYKLO-485 VMStatisticsProvider adjusted to Munin monitoring

--- ledge-components/src/main/java/org/objectledge/statistics/VMStatisticsProvider.java
+++ ledge-components/src/main/java/org/objectledge/statistics/VMStatisticsProvider.java
@@ -27,75 +27,36 @@
 // 
 package org.objectledge.statistics;
 
-import static org.objectledge.statistics.DataSource.Graph.LINE1;
-import static org.objectledge.statistics.DataSource.Type.COUNTER;
-import static org.objectledge.statistics.DataSource.Type.GAUGE;
-
 import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
 import java.util.List;
 
+import org.objectledge.filesystem.FileSystem;
+
 /**
  * A Statistics provider for the VM using JDK 5 java.lang.management interface.
  *
  * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
- * @version $Id: VMStatisticsProvider.java,v 1.11 2005-05-16 09:55:05 rafal Exp $
+ * @version $Id: VMStatisticsProvider.java,v 1.11.4.1 2008-01-17 22:56:50 rafal Exp $
  */
 public class VMStatisticsProvider
     extends ReflectiveStatisticsProvider
 {
-    private static final DataSource MEMORY_HEAP_USED_DS =
-        new DataSource("memory_heap_used", "Heap used", GAUGE, LINE1);
+    private final MuninGraph[] graphs;
     
-    private static final DataSource MEMORY_HEAP_MAX_DS =
-        new DataSource("memory_heap_max", "Heap max", GAUGE, LINE1);
-    
-    private static final DataSource MEMORY_NONHEAP_USED_DS =
-        new DataSource("memory_nonheap_used", "Non-heap used", GAUGE, LINE1);
-    
-    private static final DataSource MEMORY_NONHEAP_MAX_DS =
-        new DataSource("memory_nonheap_max", "Non-heap max", GAUGE, LINE1);
-
-    private static final Graph MEMORY_GRAPH = new Graph("memory", "Memory", null, new DataSource[] {
-                    MEMORY_HEAP_USED_DS, MEMORY_HEAP_MAX_DS, MEMORY_NONHEAP_USED_DS,
-                    MEMORY_NONHEAP_MAX_DS }, "bytes");        
-    
-    private static final DataSource GC_COUNT_VALUE_DC =
-        new DataSource("gc_count_value", "GC run count", COUNTER, LINE1);
-    
-    private static final DataSource GC_TIME_VALUE_DC = 
-        new DataSource("gc_time_value", "Total GC time", COUNTER, LINE1);
-
-    private static final Graph GC_COUNT_GRAPH = new Graph("gc_count", "Garbage collection runns",
-        null, new DataSource[] { GC_COUNT_VALUE_DC }, "runns");
-
-    private static final Graph GC_TIME_GRAPH = new Graph("gc_time", "Garbage collection time",
-        null, new DataSource[] { GC_TIME_VALUE_DC }, "milliseconds");
-    
-    private static final DataSource[] DATA_SOURCES = {
-        MEMORY_HEAP_USED_DS, MEMORY_HEAP_MAX_DS, MEMORY_NONHEAP_USED_DS,
-        MEMORY_NONHEAP_MAX_DS, GC_COUNT_VALUE_DC, GC_TIME_VALUE_DC          
-    };
-    
-    private static final Graph[] GRAPHS = {
-        MEMORY_GRAPH, GC_COUNT_GRAPH, GC_TIME_GRAPH
-    };
-    
-    /**
-     * {@inheritDoc}
-     */
-    public DataSource[] getDataSources()
+    public VMStatisticsProvider(FileSystem fs)
     {
-        return DATA_SOURCES;
+        graphs = new MuninGraph[] { new HeapMemory(fs), new NonHeapMemory(fs), new GcCount(fs),
+                        new GcTime(fs) }; 
     }
-
+    
     /**
      * {@inheritDoc}
      */
-    public Graph[] getGraphs()
+    public MuninGraph[] getGraphs()
     {
-        return GRAPHS;
+        return graphs;
     }
 
     /**
@@ -108,81 +69,138 @@ public class VMStatisticsProvider
     
     /////////////////////////////////////////////////////////////////////////////////////////////
 
-    /**
-     * Returns the size of used heap memory.
-     * 
-     * @return the size of used heap memory.
-     */
-    public Number getMemoryHeapUsed()
+    public class HeapMemory
+        extends AbstractMuninGraph
     {
-        MemoryMXBean memory = ManagementFactory.getMemoryMXBean();
-        return new Long(memory.getHeapMemoryUsage().getUsed());
+        public HeapMemory(FileSystem fs)
+        {
+            super(fs);
+        }
+
+        public String getId()
+        {
+            return "heapMemory";
+        }
+        
+        /**
+         * Returns the size of used heap memory.
+         * 
+         * @return the size of used heap memory.
+         */
+        public Number getUsed()
+        {
+            MemoryMXBean memory = ManagementFactory.getMemoryMXBean();
+            return new Long(memory.getHeapMemoryUsage().getUsed());
+        }
+        
+        /**
+         * Returns the size of free heap memory.
+         * 
+         * @return the size of free heap memory.
+         */
+        public Number getFree()
+        {
+            MemoryMXBean memory = ManagementFactory.getMemoryMXBean();
+            return new Long(memory.getHeapMemoryUsage().getMax()
+                - memory.getHeapMemoryUsage().getUsed());
+        }
     }
     
-    /**
-     * Returns the maximum size of heap memory.
-     * 
-     * @return the maximum size of heap memory.
-     */
-    public Number getMemoryHeapMax()
+    public class NonHeapMemory
+        extends AbstractMuninGraph
     {
-        MemoryMXBean memory = ManagementFactory.getMemoryMXBean();
-        return new Long(memory.getHeapMemoryUsage().getMax());
+        public NonHeapMemory(FileSystem fs)
+        {
+            super(fs);
+        }
+        
+        public String getId()
+        {
+            return "nonHeapmMemory";
+        }
+        
+        /**
+         * Returns the size of used non-heap memory.
+         * 
+         * @return the size of used non-heap memory.
+         */
+        public Number getUsed()
+        {
+            MemoryMXBean memory = ManagementFactory.getMemoryMXBean();
+            return new Long(memory.getNonHeapMemoryUsage().getUsed());
+        }
+        
+        /**
+         * Returns the size of free non-heap memory.
+         * 
+         * @return the size of free non-heap memory.
+         */
+        public Number getFree()
+        {
+            MemoryMXBean memory = ManagementFactory.getMemoryMXBean();
+            return new Long(memory.getNonHeapMemoryUsage().getMax() - memory.getNonHeapMemoryUsage().getUsed());
+        }
     }
 
-    /**
-     * Returns the size of used non-heap memory.
-     * 
-     * @return the size of used non-heap memory.
-     */
-    public Number getMemoryNonheapUsed()
+    public class GcCount
+        extends AbstractMuninGraph
     {
-        MemoryMXBean memory = ManagementFactory.getMemoryMXBean();
-        return new Long(memory.getNonHeapMemoryUsage().getUsed());
+        public GcCount(FileSystem fs)
+        {
+            super(fs);
+        }
+        
+        public String getId()
+        {
+            return "gcCount";
+        }
+        
+        /**
+         * Returns the total number of garbage collections.
+         * 
+         * @return the total number of garbage collections.
+         */
+        public Number getCount()
+        {
+            List<GarbageCollectorMXBean> garbageCollectors = 
+                ManagementFactory.getGarbageCollectorMXBeans();
+            long sum = 0;
+            for(GarbageCollectorMXBean garbageCollector : garbageCollectors)
+            {
+                sum += garbageCollector.getCollectionCount();
+            }
+            return new Long(sum);
+        }
     }
+
+    public class GcTime
+        extends AbstractMuninGraph
+    {
+        public GcTime(FileSystem fs)
+        {
+            super(fs);
+        }
+        
+        public String getId()
+        {
+            return "gcTime";
+        }
     
-    /**
-     * Returns the maximum size of non-heap memory.
-     * 
-     * @return the maximum size of non-heap memory.
-     */
-    public Number getMemoryNonheapMax()
-    {
-        MemoryMXBean memory = ManagementFactory.getMemoryMXBean();
-        return new Long(memory.getNonHeapMemoryUsage().getMax());
-    }
-
-    /**
-     * Returns the total number of garbage collections.
-     * 
-     * @return the total number of garbage collections.
-     */
-    public Number getGcCountValue()
-    {
-        List<GarbageCollectorMXBean> garbageCollectors = 
-            ManagementFactory.getGarbageCollectorMXBeans();
-        long sum = 0;
-        for(GarbageCollectorMXBean garbageCollector : garbageCollectors)
+        /**
+         * Returns the approximate accumulated garbage collection elapsed time in milliseconds.
+         * 
+         * @return the approximate accumulated garbage collection elapsed time in milliseconds.
+         */
+        public Number getTime()
         {
-            sum += garbageCollector.getCollectionCount();
+            List<GarbageCollectorMXBean> garbageCollectors = 
+                ManagementFactory.getGarbageCollectorMXBeans();
+            long sum = 0;
+            for(GarbageCollectorMXBean garbageCollector : garbageCollectors)
+            {
+                sum += garbageCollector.getCollectionTime();
+            }
+            return new Long(sum);
         }
-        return new Long(sum);
-    }
-
-    /**
-     * Returns the approximate accumulated garbage collection elapsed time in milliseconds.
-     * 
-     * @return the approximate accumulated garbage collection elapsed time in milliseconds.
-     */
-    public Number getGcTimeValue()
-    {
-        List<GarbageCollectorMXBean> garbageCollectors = 
-            ManagementFactory.getGarbageCollectorMXBeans();
-        long sum = 0;
-        for(GarbageCollectorMXBean garbageCollector : garbageCollectors)
-        {
-            sum += garbageCollector.getCollectionTime();
-        }
-        return new Long(sum);
     }
 }
