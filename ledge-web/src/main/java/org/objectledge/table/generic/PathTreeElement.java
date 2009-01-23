890e3be23e55d911f5f9d00f233fdd635db07513
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2009-01-23 10:05:21 +0000

    CYKLO-545:  generics, typos

--- ledge-web/src/main/java/org/objectledge/table/generic/PathTreeElement.java
+++ ledge-web/src/main/java/org/objectledge/table/generic/PathTreeElement.java
@@ -38,7 +38,7 @@ import java.util.Map;
  * A class that may be used for ad-hoc UI building with {@link PathTreeTableModel}.
  *
  * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
- * @version $Id: PathTreeElement.java,v 1.1 2004-02-10 17:17:46 zwierzem Exp $
+ * @version $Id: PathTreeElement.java,v 1.2 2009-01-23 10:05:21 rafal Exp $
  */
 public class PathTreeElement
 {
@@ -48,8 +48,8 @@ public class PathTreeElement
     /** Type of the element. */
     private String type;
 
-    /** Prpoperties of the element. */
-    private Map properties;
+    /** Properties of the element. */
+    private Map<String, Object> properties;
 
     /**
      * Creates a new TreeElement.
@@ -61,7 +61,7 @@ public class PathTreeElement
     {
         this.name = name;
         this.type = type;
-        this.properties = new HashMap();
+        this.properties = new HashMap<String, Object>();
     }
 
     /**
@@ -105,7 +105,7 @@ public class PathTreeElement
      * Returns the value of the TreeElement's property.
      *
      * @param property property name.
-     * @return proprety value.
+     * @return property value.
      * @see #getName()
      * @see #getType()
      */
@@ -143,14 +143,12 @@ public class PathTreeElement
      * @param property the property to compare on.
      * @return an instance of Comparator interface.
      */
-    public static Comparator getComparator(final String property)
+    public static Comparator<PathTreeElement> getComparator(final String property)
     {
-        return new Comparator()
+        return new Comparator<PathTreeElement>()
         {
-            public int compare(Object o1, Object o2)
+            public int compare(PathTreeElement e1, PathTreeElement e2)
             {
-                PathTreeElement e1 = (PathTreeElement)o1;
-                PathTreeElement e2 = (PathTreeElement)o2;
                 Comparable p1 = (Comparable)e1.get(property);
                 Comparable p2 = (Comparable)e2.get(property);
                 return p1.compareTo(p2);
@@ -164,22 +162,20 @@ public class PathTreeElement
      *
      * <p>The property must be defined (non-null) for both compared objects
      * and must be String objects. The locale must be supported by the platform's
-     * Collator implementation, as defined by Collatror.getAvailableLocales().</p>
+     * Collator implementation, as defined by Collator.getAvailableLocales().</p>
      *
      * @param property the property to compare on.
      * @param locale the locale to be used for comparisons.
      * @return an instance of Comparator interface.
      */
-    public static Comparator getComparator(final String property, final Locale locale)
+    public static Comparator<PathTreeElement> getComparator(final String property, final Locale locale)
     {
-        return new Comparator()
+        return new Comparator<PathTreeElement>()
         {
             private Collator collator = Collator.getInstance(locale);
 
-            public int compare(Object o1, Object o2)
+            public int compare(PathTreeElement e1, PathTreeElement e2)
             {
-                PathTreeElement e1 = (PathTreeElement)o1;
-                PathTreeElement e2 = (PathTreeElement)o2;
                 String s1 = (String)e1.get(property);
                 String s2 = (String)e2.get(property);
                 return collator.compare(s1, s2) ;
