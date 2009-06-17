2f5f1971b1704cfb9b43eb8aa139c388548e2dac
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2009-06-17 15:34:14 +0000

    release 1.0.3-b2

---
+++ ledge-container/src/main/java/org/objectledge/pico/customization/UnsupportedKeyTypeException.java
@@ -0,0 +1,65 @@
+// 
+// Copyright (c) 2003, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+// All rights reserved. 
+//   
+// Redistribution and use in source and binary forms, with or without modification,  
+// are permitted provided that the following conditions are met: 
+//   
+// * Redistributions of source code must retain the above copyright notice,  
+//   this list of conditions and the following disclaimer. 
+// * Redistributions in binary form must reproduce the above copyright notice,  
+//   this list of conditions and the following disclaimer in the documentation  
+//   and/or other materials provided with the distribution. 
+// * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
+//   nor the names of its contributors may be used to endorse or promote products  
+//   derived from this software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+// POSSIBILITY OF SUCH DAMAGE. 
+//
+
+package org.objectledge.pico.customization;
+
+import org.picocontainer.PicoIntrospectionException;
+
+/**
+ * Thrown by the CustomizedComponentProvider to indicate it's unable to handle a specific component
+ * key type.
+ *
+ * @author <a href="Rafal.Krzewski">rafal@caltha.pl</a>
+ * @version $Id: UnsupportedKeyTypeException.java,v 1.5 2005-02-04 02:28:15 rafal Exp $
+ */
+///CLOVER:OFF
+public class UnsupportedKeyTypeException 
+    extends PicoIntrospectionException
+{
+    /**
+     * Creates a new instance of the exception object.
+     *  
+     * @param message the detail message.
+     */
+    public UnsupportedKeyTypeException(String message)
+    {
+        super(message);
+    }
+
+    /**
+     * Creates a new instance of the exception object.
+     * 
+     * @param message detail message.
+     * @param cause the root cause.
+     */
+    public UnsupportedKeyTypeException(String message, Exception cause)
+    {
+        super(message, cause);
+    }
+
+}
---
+++ ledge-container/src/test/java/org/objectledge/pico/xml/AlternateComponentAdapterFactory.java
@@ -0,0 +1,59 @@
+// 
+// Copyright (c) 2003,2004 , Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+// All rights reserved. 
+// 
+// Redistribution and use in source and binary forms, with or without modification,  
+// are permitted provided that the following conditions are met: 
+//  
+// * Redistributions of source code must retain the above copyright notice,  
+//	 this list of conditions and the following disclaimer. 
+// * Redistributions in binary form must reproduce the above copyright notice,  
+//	 this list of conditions and the following disclaimer in the documentation  
+//	 and/or other materials provided with the distribution. 
+// * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
+//	 nor the names of its contributors may be used to endorse or promote products  
+//	 derived from this software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+// POSSIBILITY OF SUCH DAMAGE. 
+// 
+package org.objectledge.pico.xml;
+
+import org.picocontainer.ComponentAdapter;
+import org.picocontainer.Parameter;
+import org.picocontainer.PicoIntrospectionException;
+import org.picocontainer.defaults.AssignabilityRegistrationException;
+import org.picocontainer.defaults.ComponentAdapterFactory;
+import org.picocontainer.defaults.DefaultComponentAdapterFactory;
+import org.picocontainer.defaults.NotConcreteRegistrationException;
+
+/**
+ * 
+ *
+ * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
+ * @version $Id: AlternateComponentAdapterFactory.java,v 1.2 2005-02-10 17:49:00 rafal Exp $
+ */
+public class AlternateComponentAdapterFactory
+    implements ComponentAdapterFactory
+{
+    private final ComponentAdapterFactory factory = new DefaultComponentAdapterFactory();
+    
+    /**
+     * {@inheritDoc}
+     */
+    public ComponentAdapter createComponentAdapter(Object componentKey,
+        Class componentImplementation, Parameter[] parameters)
+        throws PicoIntrospectionException, AssignabilityRegistrationException,
+        NotConcreteRegistrationException
+    {
+        return factory.createComponentAdapter(componentKey, componentImplementation, parameters);
+    }
+}
