2a8df6a1a77e49baf2b1392d7fb6e9914c60ee65
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2009-08-11 20:48:17 +0000

    LEDGE-138: replaced steadystate-css2 with net.sourceforge.cssparser 0.9.5 + org.w3c.css.sac 1.3 ledge-forms in cyklotron works OK after the transition

--- ledge-forms/src/main/java/pl/caltha/forms/internal/util/CSSParser.java
+++ ledge-forms/src/main/java/pl/caltha/forms/internal/util/CSSParser.java
@@ -1,89 +1,112 @@
 package pl.caltha.forms.internal.util;
 
 // For CSS2
+import java.io.IOException;
 import java.io.StringReader;
 
-import com.steadystate.css.CSS2Parser;
-import com.steadystate.css.ParseException;
+import org.w3c.css.sac.CSSException;
+import org.w3c.css.sac.ErrorHandler;
+import org.w3c.css.sac.InputSource;
+
+import com.steadystate.css.parser.CSSOMParser;
 
 /**
- * Reads in a style string.
- *
- * $Id: CSSParser.java,v 1.2 2005-02-10 17:49:15 rafal Exp $
+ * Reads in a style string. $Id: CSSParser.java,v 1.2 2005-02-10 17:49:15 rafal Exp $
+ * 
  * @author <a href="mailto:zwierzem@ngo.pl>Damian Gajda</a>
  */
 public class CSSParser
 {
     /** Parser. */
-    private CSS2Parser fParser = null;
+    private CSSOMParser fParser = null;
 
     /** Method for parsing short style declarations embedded in a XML document. */
     public CSSStyle parse(String s)
-    throws CSSParseException
+        throws CSSParseException
     {
         CSSStyle cssStyle = null;
 
         // Check for no style
-        if ( s == null || s == "" )
+        if(s == null || s == "")
         {
             throw new CSSParseException("No style defined.");
         }
 
-        StringReader reader = new StringReader("{" + s + "}");
+        InputSource source = new InputSource(new StringReader(s));
 
-        if (fParser == null)
+        if(fParser == null)
         {
-            fParser = new CSS2Parser(reader);
-        }
-        else
-        {
-            fParser.ReInit(reader);
+            fParser = new CSSOMParser();
+            fParser.setErrorHandler(new ErrorHandlerImpl());
         }
 
         try
         {
-            cssStyle = new CSSStyle(fParser.styleDeclaration());
+            cssStyle = new CSSStyle(fParser.parseStyleDeclaration(source));
         }
-        catch (ParseException pe)
+        catch(IOException e)
         {
-            throw new CSSParseException("Error in CSS style '"+s+"'", pe);
+            throw new CSSParseException("Error in CSS style '" + s + "'", e);
         }
         return cssStyle;
     }
 
     /** test only */
-    static public void main( String[] args )
+    static public void main(String[] args)
     {
         CSSParser cssParser = new CSSParser();
 
-        String[] styleDecl =
-        { "background: black; width: 20px; list-ui: checkbox;",
-          "dizasto-d: klac; minko: 20; lig: bank;",
-          "fdfs: 54mm; yui: 20px; bui: listbox;",
-        };
+        String[] styleDecl = { "background: black; width: 20px; list-ui: checkbox;",
+                        "dizasto-d: klac; minko: 20; lig: bank;",
+                        "fdfs: 54mm; yui: 20px; bui: listbox;", };
 
-        for(int k=0; k<styleDecl.length; k++)
+        for(int k = 0; k < styleDecl.length; k++)
         {
             try
             {
-                CSSStyle style = cssParser.parse( styleDecl[k] );
+                CSSStyle style = cssParser.parse(styleDecl[k]);
 
                 System.out.println();
-                System.out.println("Style --- "+k);
+                System.out.println("Style --- " + k);
 
-                for(int i=0, j=style.getLength(); i<j; i++)
+                for(int i = 0, j = style.getLength(); i < j; i++)
                 {
                     String name = style.getPropertyName(i);
 
-                    System.out.println(name+": "+style.getProperty(name)+";");
+                    System.out.println(name + ": " + style.getProperty(name) + ";");
                 }
             }
-            catch (CSSParseException e)
+            catch(CSSParseException e)
             {
                 System.out.println(e.getMessage());
                 e.printStackTrace();
                 return;
             }
+        }
+    }
+
+    private static class ErrorHandlerImpl
+        implements ErrorHandler
+    {
+        @Override
+        public void warning(org.w3c.css.sac.CSSParseException arg0)
+            throws CSSException
+        {
+            // ignore
+        }
+
+        @Override
+        public void fatalError(org.w3c.css.sac.CSSParseException arg0)
+            throws CSSException
+        {
+            throw arg0;
+        }
+
+        @Override
+        public void error(org.w3c.css.sac.CSSParseException arg0)
+            throws CSSException
+        {
+            throw arg0;
         }
     }
 }
