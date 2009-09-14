1639e7b802182c0b6aa7d319fd9dc85b33cd40eb
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2009-09-14 22:08:12 +0000

    release 1.0.3.1

---
+++ ledge-forms/src/main/java/pl/caltha/forms/internal/ui/NodeForm.java
@@ -0,0 +1,89 @@
+package pl.caltha.forms.internal.ui;
+
+import org.xml.sax.Attributes;
+
+import pl.caltha.forms.ConstructionException;
+import pl.caltha.forms.internal.model.InstanceImpl;
+import pl.caltha.forms.internal.model.SubmitInfo;
+
+/**
+ * Represents ...
+ *
+ * @author <a href="mailto:zwierzem@ngo.pl">Damian Gajda</a>
+ * @version $Id: NodeForm.java,v 1.1 2005-01-19 06:55:28 pablo Exp $
+ */
+public class NodeForm extends NodeSelectableContainer
+{
+    public NodeForm(String type, Attributes atts)
+    throws ConstructionException
+    {
+        super(type, atts);
+        ref = new ReferenceSingleForm(atts, this);
+    }
+
+    /** UI containing this form node. */
+    private UI ui;
+    /** Connceted submit info. */
+    private SubmitInfo submitInfo;
+
+    //------------------------------------------------------------------------
+    // Form methods
+    //
+    /** Returns actual page, depending on Instance. */
+    public NodeSelectable getPage(InstanceImpl instance)
+    {
+        return getCurrentChild(instance);
+    }
+
+    //------------------------------------------------------------------------
+    // XForms properties
+    public String getMethod()
+    {
+        return submitInfo.getMethod();
+    }
+
+    public String getEnctype()
+    {
+        return submitInfo.getEncType();
+    }
+
+    public String getAcceptCharset()
+    {
+        return submitInfo.getEncoding();
+    }
+
+    public String getAcceptMIME()
+    {
+        return submitInfo.getMediaType();
+    }
+
+    //------------------------------------------------------------------------
+    // HTML properties
+
+    public String getOnSubmit()
+    {
+        // TODO: not implemented
+        return null;
+    }
+
+    public String getOnReset()
+    {
+        // TODO: not implemented
+        return null;
+    }
+    //------------------------------------------------------------------------
+    // methods used during building
+    //
+
+    /** Inits a Node.
+     * @throws ConstructionException Thrown on initialisation errors
+     */
+    protected void init(UI ui)
+    throws ConstructionException
+    {
+        super.init(ui);
+        this.ui = ui;
+        submitInfo = ui.getForm().getSubmitInfo();
+    }
+}
+
---
+++ ledge-forms/src/main/java/pl/caltha/forms/internal/ui/NodeRepeatSubTree.java
@@ -0,0 +1,57 @@
+package pl.caltha.forms.internal.ui;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.xml.sax.Attributes;
+
+import pl.caltha.forms.ConstructionException;
+import pl.caltha.forms.internal.util.Util;
+
+/**
+ * Represents ...
+ *
+ * @author <a href="mailto:zwierzem@ngo.pl">Damian Gajda</a>
+ * @version $Id: NodeRepeatSubTree.java,v 1.1 2005-01-19 06:55:28 pablo Exp $
+ */
+public class NodeRepeatSubTree extends NodeCaptionReference
+{
+    public NodeRepeatSubTree(String type, Attributes atts)
+    throws ConstructionException
+    {
+        super(type, atts);
+    }
+
+    /** Used when copying parts of UI tree for repeat nodes processing.
+     * <p>Fields which are copied and cleared:</p>
+     * <ul>
+     *  <li>{@link #nodesById} - it must be a new HashMap so that it indexes
+     *  only nodes from a cloned subtree</li>
+     * </ul>
+     */
+    protected Object clone()
+    {
+        NodeRepeatSubTree next = (NodeRepeatSubTree)(super.clone());
+        next.nodesById = new HashMap();
+        return next;
+    }
+
+    private HashMap nodesById = new HashMap();
+
+    /** Returns node(s) with a given ID. */
+    public List getNodesById(String id)
+    {
+        return (List)(nodesById.get(id));
+    }
+
+    //------------------------------------------------------------------------
+    // methods used during building
+
+    void addNodeById(Node node)
+    throws ConstructionException
+    {
+        // we do not have to check for ID identity
+        // it is already done in UI
+        Util.insertMultipleIntoHash(node.id, node, nodesById);
+    }
+}
