71a8bcf0d5e6af35754d9605f602ce794e05d8dc
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2009-09-22 21:14:59 +0000

    release 1.0.3.2

---
+++ ledge-forms/src/main/java/pl/caltha/forms/internal/ui/NodeControlSelect.java
@@ -0,0 +1,139 @@
+package pl.caltha.forms.internal.ui;
+
+import java.util.HashMap;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+import org.xml.sax.Attributes;
+
+import pl.caltha.forms.ConstructionException;
+import pl.caltha.forms.internal.model.InstanceImpl;
+import pl.caltha.forms.internal.util.Util;
+
+/**
+ * Implementation of Select controls.
+ *
+ * @author <a href="mailto:zwierzem@ngo.pl">Damian Gajda</a>
+ * @version $Id: NodeControlSelect.java,v 1.2 2006-04-28 10:02:23 pablo Exp $
+ */
+public class NodeControlSelect extends NodeControl
+{
+
+    public NodeControlSelect(String type, Attributes atts)
+    throws ConstructionException
+    {
+        super(type, atts);
+
+        selectUI = Util.getSAXAttributeVal(atts, "selectUI");
+        size = Util.createIntAttribute(atts, "size", 1);
+        key = Util.createAttribute(atts, "key", "empty");
+    }
+
+    private HashMap itemsByValue = new HashMap();
+
+    void addItemByValue(NodeControlSelectItem item)
+    {
+        if(!this.itemsByValue.containsValue(item))
+        {
+            this.itemsByValue.put(item.getValue(), item);
+        }
+    }
+
+    //------------------------------------------------------------------------
+    //attributes
+    /** UI type for Select control. */
+    private String selectUI;
+    private int size;
+    private String key;
+
+    //------------------------------------------------------------------------
+    //access methods for attributes
+    public String getSelectUI()
+    {
+        return selectUI;
+    }
+
+    public int getSize()
+    {
+        return size;
+    }
+    
+    public String getKey()
+    {
+        return key;
+    }
+
+    /** Returns true if given item is selected in a given instance.
+     * This method is called by select items. */
+    boolean getSelected(InstanceImpl instance, NodeControlSelectItem item)
+    {
+        Set selectedValues = getSelectedValuesSet(instance);
+        // Return true for selected item/value
+        return selectedValues.contains(item.getValue());
+    }
+
+    public boolean isSelected(InstanceImpl instance, String value)
+    {
+        Set selectedValues = getSelectedValuesSet(instance);
+        // Return true for selected item/value
+        return selectedValues.contains(value);
+    }
+    
+    /** Key for storing selected values map for this Select element.
+     * Selected values is not connected with definitionUIPath, because
+     * it is an instance property as opposed to UI state property
+     */
+    protected String SELECTED_VALUES = "select.selectedValues";
+
+    private Set getSelectedValuesSet(InstanceImpl instance)
+    {
+        org.dom4j.Node contextNode = ((ReferenceSingle)ref).getContextNode(instance);
+        Set selectedValues = (Set)(instance.getStateValue(contextNode, SELECTED_VALUES));
+
+        // 1. build selected values if null
+        if(selectedValues == null)
+        {
+            selectedValues = new java.util.HashSet();
+            // 1.1. set value from instance
+            String value = ref.getValue(contextNode);
+            setSelectedValuesValue(selectedValues, value);
+            // 1.2. Store selectedValues in state
+            instance.setStateValue(contextNode, SELECTED_VALUES, selectedValues);
+        }
+
+        // 2. return selected values
+        return selectedValues;
+    }
+
+    private void setSelectedValuesValue(Set selectedValues, String value)
+    {
+        // check for null value and set it
+        if(value != null)
+        {
+            // It Tokenizes a value ONCE for this contextNode
+            // it speeds up getSelected
+            StringTokenizer tokenizer = new StringTokenizer(value);
+
+            while(tokenizer.hasMoreTokens())
+            {
+                selectedValues.add(tokenizer.nextToken());
+            }
+        }
+    }
+
+    //------------------------------------------------------------------------
+    // Control methods
+    //
+    void setValue(InstanceImpl instance, String value)
+    {
+        // 1. Set value in instance
+        super.setValue(instance, value);
+
+        // 2. Get selected values
+        Set selectedValues = getSelectedValuesSet(instance);
+        // 3. clean old value
+        selectedValues.clear();
+        // 4. set new value
+        setSelectedValuesValue(selectedValues, value);
+    }
+}
---
+++ ledge-forms/src/main/java/pl/caltha/internal/xml/XMLDataReaderImpl.java
@@ -0,0 +1,230 @@
+package pl.caltha.internal.xml;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.io.SAXReader;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.DTDHandler;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+import pl.caltha.services.xml.XMLDataReader;
+
+import com.sun.msv.verifier.VerifierFilter;
+
+/**
+ * An implemetation of XMLDataReader.
+ *
+ * @author <a href="mailto:zwierzem@ngo.pl">Damian Gajda</a>
+ * @version $Id: XMLDataReaderImpl.java,v 1.1 2005-01-20 16:44:47 pablo Exp $
+ */
+public class XMLDataReaderImpl
+    implements XMLDataReader
+{
+    public XMLDataReaderImpl(XMLServiceImpl xmlService)
+    throws SAXException, ParserConfigurationException
+    {
+        this.xmlService = xmlService;
+        reader = xmlService.getFactory().newSAXParser().getXMLReader();
+        entityResolver = xmlService.getEntityResolver();
+        saxPipe = new java.util.ArrayList(3);
+    }
+
+    private XMLServiceImpl xmlService;
+    private XMLReader reader;
+
+    private EntityResolver entityResolver;
+    private DTDHandler dtdHandler;
+    private java.util.ArrayList saxPipe;
+
+    //------------------------------------------------------------------------
+    // XMLDataReader methods
+    
+    public Document readDOM4J(InputSource is, String schemaID,
+                         ErrorHandler errorHandler)
+    throws SAXException, ParserConfigurationException, Exception
+    {
+        if(schemaID != null)
+        {
+            return readDOM4JValidate(is, schemaID, errorHandler);
+        }
+
+        XMLReader xmlReader = setupReader();
+        xmlReader.setErrorHandler(errorHandler);
+        return readFinally(xmlReader, is);
+    }
+
+    public void readSAX(InputSource is, String schemaID,
+                        ContentHandler contentHandler,
+                        ErrorHandler errorHandler)
+    throws SAXException, ParserConfigurationException, Exception
+    {
+        if(schemaID != null)
+        {
+            readSAXValidate(is, schemaID, contentHandler, errorHandler);
+            return;
+        }
+
+        setupReader();
+        reader.setErrorHandler(errorHandler);
+        reader.setContentHandler(contentHandler);
+        readSAXFinally(reader, is);
+    }
+
+    public EntityResolver getEntityResolver()
+    {
+        return entityResolver;
+    }
+
+    public void setEntityResolver(EntityResolver entityResolver)
+    {
+        this.entityResolver = entityResolver;
+    }
+    
+    public DTDHandler getDTDHandler()
+    {
+        return dtdHandler;
+    }
+    
+    public void setDTDHandler(DTDHandler dtdHandler)
+    {
+        this.dtdHandler = dtdHandler;
+    }
+
+    public void addFilter(org.xml.sax.XMLFilter xmlFilter)
+    {
+        saxPipe.add(xmlFilter);
+    }
+
+    public void removeFilters()
+    {
+        saxPipe.clear();
+    }
+
+    //------------------------------------------------------------------------
+    // Implementation methods
+    
+    /** Sets up a SAX processing pipe.
+     * Connects XMLFilters, EntityResolver and a DTDHandler. */
+    private XMLReader setupReader()
+    {
+        // TODO: get one from the pool
+        // TODO: after reading return one to the pool
+        //reader = get one from pool
+        
+        if(entityResolver != null)
+        {
+            reader.setEntityResolver(entityResolver);
+        }
+        if(dtdHandler != null)
+        {
+            reader.setDTDHandler(dtdHandler);
+        }
+     
+        XMLReader lastReader = this.reader;
+        for(java.util.Iterator iter =  saxPipe.iterator(); iter.hasNext();)
+        {
+            org.xml.sax.XMLFilter filter = (org.xml.sax.XMLFilter)(iter.next());
+            filter.setParent(lastReader);
+            lastReader = filter;
+        }
+        
+        return lastReader;
+    }
+    
+    /** Connects a VerifierFilter to SAX processing pipe.
+     * If <code>DTDHandler</code> is <code>null</code>
+     * <code>VerifierFilter</code> is used as such. */
+    private VerifierFilter setupVerifierFilter(XMLReader xmlReader, String schemaID, ErrorHandler errorHandler)
+    throws Exception
+    {
+        VerifierFilter verifierFilter = xmlService.getVerifierFilter(schemaID, errorHandler);
+        // WARN DTDHandler is set on original XMLReader
+        if(dtdHandler == null)
+        {
+            reader.setDTDHandler(verifierFilter);
+        }
+        verifierFilter.setParent(xmlReader);
+        verifierFilter.setErrorHandler(errorHandler);
+        return verifierFilter;
+    }
+   
+    /** Reads XML data and returns a DOM4J Document. Uses schema for validation.
+     * @param is InputSource for data to be read.
+     * @param schemaID Public or System ID of schema defining read XML data.
+     * @param errorHandler SAX ErrorHandler to handle errors from read XML file.
+     * @throws SAXException Thrown on problems with parsing XML data.
+     * @throws ParserConfigurationException Thrown on problems with JAXP parser configuration.
+     * @throws Exception Validation and other Exceptions.
+     * @return DOM4J Document object representing read XML data.
+     */
+    private Document readDOM4JValidate(InputSource is, String schemaID,
+                                 ErrorHandler errorHandler)
+    throws SAXException, ParserConfigurationException, Exception
+    {
+        XMLReader xmlReader = setupReader();
+        VerifierFilter verifierFilter = setupVerifierFilter(xmlReader, schemaID, errorHandler);
+        Document doc = readFinally(verifierFilter, is);
+
+        if(verifierFilter.isValid())
+        {
+            return doc;
+        }
+        else
+        {
+            throw new DocumentException("The document was invalid.");
+        }
+    }
+
+
+
+    private Document readFinally(XMLReader xmlReader, InputSource is)
+    throws DocumentException
+    {
+        SAXReader saxReader = new SAXReader(xmlReader);
+
+        saxReader.setStripWhitespaceText(true);
+        // it gives errors on DOM Validation.
+        //saxReader.setMergeAdjacentText(true);
+
+        return saxReader.read(is);
+    }
+
+
+    /** Reads XML data and generates SAX events. Uses schema for validation.
+     * @param is InputSource for data to be read.
+     * @param schemaID Public or System ID of schema defining read XML data.
+     * @param contentHandler SAX ContentHandler to handle generated SAX events.
+     * @param errorHandler SAX ErrorHandler to handle errors from read XML data.
+     * @throws SAXException Thrown on problems with parsing XML data.
+     * @throws ParserConfigurationException Thrown on problems with JAXP parser configuration.
+     * @throws Exception Validation and other Exceptions.
+     */
+    private void readSAXValidate(InputSource is, String schemaID,
+                                ContentHandler contentHandler,
+                                ErrorHandler errorHandler)
+    throws SAXException, ParserConfigurationException, Exception
+    {
+        XMLReader xmlReader = setupReader();
+        VerifierFilter verifierFilter = setupVerifierFilter(xmlReader, schemaID, errorHandler);
+        verifierFilter.setContentHandler(contentHandler);
+        readSAXFinally(verifierFilter, is);
+
+        if(!verifierFilter.isValid())
+        {
+            throw new SAXException("The document '"+is.getSystemId()+"' was invalid");
+        }
+    }
+
+
+    private void readSAXFinally(XMLReader reader, InputSource is)
+    throws Exception
+    {
+        reader.parse(is);
+    }
+}
