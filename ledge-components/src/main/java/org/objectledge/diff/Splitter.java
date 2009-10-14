9113aa15e96f62e87d6fb11f00c1a49fbf4c6f4b
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2009-10-14 20:45:06 +0000

    merging changes from 1.0.3 branch - added whitespace splitter

--- ledge-components/src/main/java/org/objectledge/diff/Splitter.java
+++ ledge-components/src/main/java/org/objectledge/diff/Splitter.java
@@ -8,6 +8,8 @@ public interface Splitter
     
     public static final Splitter WORD_BOUNDARY_SPLITTER = new RegexSplitter("\\b");
     
+    public static final Splitter SP_SPLITTER = new RegexSplitter("\\s+");
+
     public static final Splitter CHARACTER_SPLITER = new RegexSplitter("");
 
     public List<String> split(String string);
