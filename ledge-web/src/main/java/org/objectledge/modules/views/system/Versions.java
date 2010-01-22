78ef2e683764cc90717140d97fcc1f9e0499ab61
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2010-01-22 10:08:28 +0000

    release 1.0.4-b1

---
+++ ledge-web/src/main/java/org/objectledge/modules/views/system/Versions.java
@@ -0,0 +1,132 @@
+// 
+// Copyright (c) 2003-2005, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+// All rights reserved. 
+// 
+// Redistribution and use in source and binary forms, with or without modification,  
+// are permitted provided that the following conditions are met: 
+//  
+// * Redistributions of source code must retain the above copyright notice,  
+//	 this list of conditions and the following disclaimer. 
+// * Redistributions in binary form must reproduce the above copyright notice,  
+//	 this list of conditions and the following disclaimer in the documentation  
+//	 and/or other materials provided with the distribution. 
+// * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
+//	 nor the names of its contributors may be used to endorse or promote products  
+//	 derived from this software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+// POSSIBILITY OF SUCH DAMAGE. 
+// 
+package org.objectledge.modules.views.system;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import org.objectledge.context.Context;
+import org.objectledge.filesystem.FileSystem;
+import org.objectledge.pipeline.ProcessingException;
+import org.objectledge.templating.TemplatingContext;
+import org.objectledge.web.mvc.builders.PolicyProtectedBuilder;
+import org.objectledge.web.mvc.security.PolicySystem;
+
+/**
+ * 
+ *
+ * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
+ * @version $Id: Versions.java,v 1.4 2005-07-26 12:13:29 rafal Exp $
+ */
+public class Versions
+    extends PolicyProtectedBuilder
+{
+    private final FileSystem fileSystem;
+    
+    /**
+     * Creates new Versions view instance.
+     * 
+     * @param fileSystemArg the FileSystem component.
+     * @param contextArg the Context component.
+     * @param policySystemArg the PolicySystem component.
+     */
+    public Versions(FileSystem fileSystemArg, Context contextArg, PolicySystem policySystemArg)
+    {
+        super(contextArg, policySystemArg);
+        fileSystem = fileSystemArg;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void process(TemplatingContext templatingContext) 
+        throws ProcessingException
+    {
+        try
+        {
+            List<List<String>> temp = new ArrayList<List<String>>();
+            String dir = "/META-INF/versions/";
+            if(fileSystem.exists(dir) && fileSystem.isDirectory(dir) && fileSystem.canRead(dir))
+            {
+                String[] groups = fileSystem.list(dir);
+                if(groups != null)
+                {
+                    Arrays.sort(groups);
+                    for(int i = 0; i < groups.length; i++)
+                    {
+                        String[] artifacts = fileSystem.list(dir + groups[i]);
+                        if(artifacts != null)
+                        {
+                            Arrays.sort(artifacts);
+                            for(int j = 0; j < artifacts.length; j++)
+                            {
+                                String versionString = fileSystem.read(dir + groups[i] + "/"
+                                    + artifacts[j], "UTF-8"); 
+                                temp.add(Arrays.asList(versionString.split("/")));
+                            }
+                        }
+                    }
+                }
+            }
+            templatingContext.put("versions", temp);
+        }
+        catch(IOException e)
+        {
+            throw new ProcessingException("FS access failed", e);
+        }
+        
+        /////////////////////////////////////////////////////////////////////////////////////////
+
+        Package[] packages = Package.getPackages();
+        Arrays.sort(packages, new Comparator<Package>()
+            {
+                public int compare(Package p1, Package p2)
+                {
+                    return p1.getName().compareTo(p2.getName());
+                }
+            });
+        List<Package> topLevelPackages = new ArrayList<Package>();
+        outer: for(int i=0; i<packages.length; i++)
+        {
+            for(int j=i-1; j>=0; j--)
+            {
+                if(packages[i].getName().startsWith(packages[j].getName()))
+                {
+                    continue outer;
+                }
+            }
+            topLevelPackages.add(packages[i]);
+        }
+        templatingContext.put("packages", topLevelPackages);
+    }        
+    
+}
