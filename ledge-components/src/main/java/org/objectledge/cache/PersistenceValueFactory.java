b44b016f069958eb9e588221cecd10dcbbfeef54
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2010-06-27 23:07:30 +0000

    release 1.0.6.1

---
+++ ledge-components/src/main/java/org/objectledge/cache/PersistenceValueFactory.java
@@ -0,0 +1,139 @@
+//
+//Copyright (c) 2003, Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.
+//All rights reserved.
+//
+//Redistribution and use in source and binary forms, with or without modification, 
+//are permitted provided that the following conditions are met:
+//
+//* Redistributions of source code must retain the above copyright notice, 
+//this list of conditions and the following disclaimer.
+//* Redistributions in binary form must reproduce the above copyright notice, 
+//this list of conditions and the following disclaimer in the documentation 
+//and/or other materials provided with the distribution.
+//* Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+//nor the names of its contributors may be used to endorse or promote products 
+//derived from this software without specific prior written permission.
+//
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+//AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+//WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+//INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+//BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+//OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+//WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+//ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+//POSSIBILITY OF SUCH DAMAGE.
+//
+
+package org.objectledge.cache;
+
+import org.jcontainer.dna.Configuration;
+import org.objectledge.cache.spi.CacheFactorySPI;
+import org.objectledge.cache.spi.ConfigurableValueFactory;
+import org.objectledge.database.persistence.Persistence;
+import org.objectledge.database.persistence.PersistenceException;
+import org.objectledge.database.persistence.Persistent;
+import org.objectledge.database.persistence.PersistentFactory;
+
+/**
+ * An implementation of ValueFactory interface that uses the persistency.
+ * 
+ * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
+ * @version $Id: PersistenceValueFactory.java,v 1.5 2005-02-10 17:49:17 rafal Exp $
+ */
+public class PersistenceValueFactory<K extends Number, V extends Persistent>
+    implements ConfigurableValueFactory<K, V>
+{
+    /**
+     * The persistent factory.
+     */
+    private PersistentFactory<V> factory;
+
+    /**
+     * The persistence.
+     */
+    private Persistence persistence;
+
+    /**
+     * Initializes the factory.
+     *
+     * @param persistence the persistence.
+     * @param cl the class of the values.
+     */
+    public void init(final Class<V> cl, Persistence persistence)
+    {
+        this.persistence = persistence;
+        if(!Persistent.class.isAssignableFrom(cl))
+        {
+            throw new IllegalArgumentException(cl.getName()+" does not implement " +                                                "Persistent interface");
+        }
+        factory = new PersistentFactory<V>()
+            {
+                public V newInstance()
+                    throws PersistenceException
+                {
+                    try
+                    {
+                        return cl.newInstance();
+                    }
+                    catch(Exception e)
+                    {
+                        throw new PersistenceException("failed to instantiate "+cl.getName());
+                    }
+                }
+            };
+    }
+
+    /**
+     * Produces a value that corresponds to a given key.
+     *
+     * <p>This metod expects that the key extends the
+     * <code>java.lang.Number</code> interface.</p>
+     * 
+     * @param key the key.
+     * @return the object.
+     */
+    public V getValue(K key)
+    {
+        try
+        {
+            return persistence.load(key.longValue(), factory);
+        }
+        catch(PersistenceException e)
+        {
+            throw new RuntimeException("failed to produce value", e);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @SuppressWarnings("unchecked")
+    public void configure(CacheFactorySPI caching, String name, Configuration config)
+    {
+        Configuration[] parameters = config.getChildren("parameter");
+        String vClass = null;
+        for(int i = 0; i < parameters.length; i++)
+        {
+            if(parameters[i].getAttribute("name","").equals("valueClass"))
+            {
+                vClass = parameters[i].getAttribute("value","");
+            }
+        }
+        if(vClass == null || vClass.length()==0)
+        {
+            throw new IllegalArgumentException("required parameter valueClass is missing");
+        }
+        Class<V> cl = null;
+        try
+        {
+            cl = (Class<V>)Class.forName(vClass);
+        }
+        catch(Exception e)
+        {
+            throw new IllegalArgumentException("cannot instantiate the class"+e.getMessage());
+        }
+        init(cl, caching.getPersistence());
+    }
+}
---
+++ ledge-components/src/main/java/org/objectledge/logging/LedgeLoggingEvent.java
@@ -0,0 +1,115 @@
+// 
+// Copyright (c) 2003,2004 , Caltha - Gajda, Krzewski, Mach, Potempski Sp.J. 
+// All rights reserved. 
+// 
+// Redistribution and use in source and binary forms, with or without modification,  
+// are permitted provided that the following conditions are met: 
+//  
+// * Redistributions of source code must retain the above copyright notice,  
+//	 this list of conditions and the following disclaimer. 
+// * Redistributions in binary form must reproduce the above copyright notice,  
+//	 this list of conditions and the following disclaimer in the documentation  
+//	 and/or other materials provided with the distribution. 
+// * Neither the name of the Caltha - Gajda, Krzewski, Mach, Potempski Sp.J.  
+//	 nor the names of its contributors may be used to endorse or promote products  
+//	 derived from this software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED  
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,  
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  
+// POSSIBILITY OF SUCH DAMAGE. 
+// 
+package org.objectledge.logging;
+
+import org.apache.log4j.Category;
+import org.apache.log4j.Priority;
+import org.apache.log4j.spi.LoggingEvent;
+import org.apache.log4j.spi.ThrowableInformation;
+
+/**
+ * LoggingEvent subclass, created to plug in {@link org.objectledge.utils.StackTrace} throwable 
+ * formatter.
+ *
+ * @author <a href="mailto:rafal@caltha.pl">Rafal Krzewski</a>
+ * @version $Id: LedgeLoggingEvent.java,v 1.3 2005-02-08 00:35:24 rafal Exp $
+ */
+public class LedgeLoggingEvent
+	extends LoggingEvent
+{
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private LedgeThrowableInformation ledgeThrowableInfo = null;
+    
+    /**
+     * Creates new LedgeLoggingEvent instance.
+     * 
+     * @param fqnOfCategoryClass the fully qualified name of the logger class.
+     * @param logger the logger object.
+     * @param timeStamp time stamp of the event.
+     * @param priority event priority.
+     * @param message the message.
+     * @param throwable the attached throwable if any.
+     */
+    public LedgeLoggingEvent(String fqnOfCategoryClass, Category logger, long timeStamp,
+        Priority priority, Object message, Throwable throwable)
+    {
+        super(fqnOfCategoryClass, logger, timeStamp, priority, message, throwable);
+        if(throwable != null)
+        {
+            ledgeThrowableInfo = new LedgeThrowableInformation(throwable);
+        }
+    }
+
+    /**
+     * Creates new LedgeLoggingEvent instance.
+     * 
+     * @param fqnOfCategoryClass the fully qualified name of the logger class.
+     * @param logger the logger object.
+     * @param priority event priority.
+     * @param message the message.
+     * @param throwable the attached throwable if any.
+     */
+    public LedgeLoggingEvent(String fqnOfCategoryClass, Category logger, Priority priority,
+        Object message, Throwable throwable)
+    {
+        super(fqnOfCategoryClass, logger, priority, message, throwable);
+        if(throwable != null)
+        {
+            ledgeThrowableInfo = new LedgeThrowableInformation(throwable);
+        }
+    }
+
+
+    /**
+     * Returns the throwable information contained within this event. May be <code>null</code> if
+     * there is no such information.
+     * 
+     * <p> Note that the {@link Throwable}object contained within a {@link ThrowableInformation}does
+     * not survive serialization. </p>
+     * 
+     * @return ThrowableInformation contained within this event.
+     */
+    public ThrowableInformation getThrowableInformation() 
+    {
+        return ledgeThrowableInfo;
+    }
+    
+    
+    /**
+     * Return this event's throwable's string[] representation.
+     * 
+     * @return this event's throwable's string[] representation.
+     */
+    public String[] getThrowableStrRep()
+    {
+        return ledgeThrowableInfo != null ? ledgeThrowableInfo.getThrowableStrRep() : null;
+    }
+}
