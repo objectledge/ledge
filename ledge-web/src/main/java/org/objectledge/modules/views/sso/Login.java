58d4fcc5f9e8b51d5291b4e2e7720e78344f3063
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2011-07-18 15:47:45 +0000

    CYKLO-523 JSON view for remote SSO logins

---
+++ ledge-web/src/main/java/org/objectledge/modules/views/sso/Login.java
@@ -0,0 +1,109 @@
+package org.objectledge.modules.views.sso;
+
+import java.security.Principal;
+
+import org.jcontainer.dna.Logger;
+import org.objectledge.authentication.AuthenticationException;
+import org.objectledge.authentication.SingleSignOnService;
+import org.objectledge.authentication.UserManager;
+import org.objectledge.authentication.UserUnknownException;
+import org.objectledge.context.Context;
+import org.objectledge.parameters.Parameters;
+import org.objectledge.parameters.RequestParameters;
+import org.objectledge.pipeline.ProcessingException;
+import org.objectledge.templating.Template;
+import org.objectledge.web.HttpContext;
+import org.objectledge.web.mvc.builders.AbstractBuilder;
+import org.objectledge.web.mvc.builders.BuildException;
+import org.objectledge.web.mvc.builders.EnclosingView;
+
+public class Login
+    extends AbstractBuilder
+{
+    /** login parameter name. */
+    public static final String LOGIN_PARAM = "login";
+
+    /** password parameter name. */
+    public static final String PASSWORD_PARAM = "password";
+
+    private final SingleSignOnService singleSignOnService;
+
+    private final UserManager userManager;
+
+    private final Logger log;
+
+    public Login(UserManager userManager, SingleSignOnService singleSignOnService, Context context,
+        Logger log)
+    {
+        super(context);
+        this.userManager = userManager;
+        this.singleSignOnService = singleSignOnService;
+        this.log = log;
+    }
+
+    @Override
+    public String build(Template template, String embeddedBuildResults)
+        throws BuildException, ProcessingException
+    {
+        Parameters parameters = context.getAttribute(RequestParameters.class);
+        HttpContext httpContext = context.getAttribute(HttpContext.class);
+        String client = httpContext.getRequest().getRemoteAddr();
+        String domain = httpContext.getRequest().getServerName();
+        String login = parameters.get(LOGIN_PARAM, null);
+        String password = parameters.get(PASSWORD_PARAM, null);
+
+        String status = "success";
+        String ticket = null;
+
+        if(!httpContext.getRequest().isSecure())
+        {
+
+            Principal principal = null;
+            try
+            {
+                principal = userManager.getUserByLogin(login);
+                if(!userManager.checkUserPassword(principal, password))
+                {
+                    log.warn("DECLINED " + client + " login " + login + " invalid password");
+                    status = "invalid_credentials";
+                }
+                ticket = singleSignOnService.generateTicket(principal, domain, client);
+                if(ticket == null)
+                {
+                    // domain is not a realm master
+                    status = "invalid_request";
+                }
+            }
+            catch(UserUnknownException e)
+            {
+                log.warn("DECLINED " + client + " unknown user login " + login);
+                status = "invalid_credentials";
+            }
+            catch(AuthenticationException e)
+            {
+                log.error("DECLINED " + client + " login " + login + " AuthenticationException ", e);
+                status = "internal_error";
+            }
+        }
+        else
+        {
+            log.warn("DECLINED " + client + " login " + login + " not using secure channel");
+            status = "invalid_request";
+        }
+
+        httpContext.setContentType("application/json");
+        return formatReply(status, ticket);
+    }
+
+    private String formatReply(String status, String ticket)
+    {
+        return "{ status : \"" + status + "\",\n ticket : \"" + ticket != null ? ticket : "NONE"
+            + "\" }";
+    }
+
+    @Override
+    public EnclosingView getEnclosingView(String thisViewName)
+    {
+        return EnclosingView.TOP;
+    }
+}
