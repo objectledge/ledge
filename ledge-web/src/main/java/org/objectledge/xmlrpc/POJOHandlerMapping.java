e298741e50103f8c7eab1086af72f05f60eabc6d
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2011-08-18 12:06:15 +0000

    CYKLO-523 alternative constructor that allows exporting void methods

--- ledge-web/src/main/java/org/objectledge/xmlrpc/POJOHandlerMapping.java
+++ ledge-web/src/main/java/org/objectledge/xmlrpc/POJOHandlerMapping.java
@@ -7,7 +7,7 @@ import org.apache.xmlrpc.server.RequestProcessorFactoryFactory;
 /**
  * XmlRpcHandlerMapping suitable for singleton POJO XmlRpc handlers.
  * <p>
- * All methods in a selected interface implemented by the handler are registered.
+ * All methods in a selected interfaces implemented by the handler are registered.
  * </p>
  * <p>
  * All requests are processed using the same handler instance. Handler needs to be implemented in a
@@ -19,18 +19,34 @@ import org.apache.xmlrpc.server.RequestProcessorFactoryFactory;
  */
 public class POJOHandlerMapping
     extends AbstractReflectiveHandlerMapping
-{
+{    
     /**
      * Creates a new POJOHandlerMapping.
      * 
-     * @param handler
-     * @param publicInterfaces
+     * @param handler the singleton handler object.
+     * @param voidMethodsEnabled should void methods be exposed {@see AbstractReflectiveHandlerMapping#setVoidMethodEnabled(boolean)}.
+     * @param publicInterfaces interfaces implemented by the handled object that should be exposed trough XmlRpc.
      * @throws XmlRpcException
      */
     public POJOHandlerMapping(Object handler, Class<? >... publicInterfaces)
         throws XmlRpcException
+    {
+        this(handler, false, publicInterfaces);
+    }
+    
+    /**
+     * Creates a new POJOHandlerMapping.
+     * 
+     * @param handler the singleton handler object.
+     * @param voidMethodsEnabled should void methods be exposed {@see AbstractReflectiveHandlerMapping#setVoidMethodEnabled(boolean)}.
+     * @param publicInterfaces interfaces implemented by the handled object that should be exposed trough XmlRpc.
+     * @throws XmlRpcException
+     */
+    public POJOHandlerMapping(Object handler, boolean voidMethodsEnabled, Class<? >... publicInterfaces)
+        throws XmlRpcException
     {        
         setRequestProcessorFactoryFactory(new POJORequestProcessorFactoryFactory(handler));
+        setVoidMethodEnabled(voidMethodsEnabled);
         for(Class<? > publicInterface : publicInterfaces)
         {
             if(!publicInterface.isAssignableFrom(handler.getClass()))
