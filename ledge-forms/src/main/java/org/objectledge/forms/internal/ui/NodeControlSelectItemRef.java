138b516e4a49b214d5f3ea129bb6b99a5769421a
Rafał Krzeswski <rafal.krzewski@caltha.pl>
2011-10-13 10:36:12 +0000

    release 1.0.11.2

---
+++ ledge-forms/src/main/java/org/objectledge/forms/internal/ui/NodeControlSelectItemRef.java
@@ -0,0 +1,21 @@
+package org.objectledge.forms.internal.ui;
+
+import org.objectledge.forms.ConstructionException;
+import org.xml.sax.Attributes;
+
+
+/**
+ * TODO: Implement it
+ *
+ * @author <a href="mailto:zwierzem@ngo.pl">Damian Gajda</a>
+ * @version $Id: NodeControlSelectItemRef.java,v 1.1 2005-01-19 06:55:28 pablo Exp $
+ */
+public class NodeControlSelectItemRef extends NodeControlSelectItem
+{
+    public NodeControlSelectItemRef(String type, Attributes atts)
+    throws ConstructionException
+    {
+        super(type, atts);
+    }
+}
+
