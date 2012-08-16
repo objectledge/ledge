0489461f3cf9db6294fd63f8986ef11c6c83f6a7
Rafał Krzewski <rafal.krzewski@objectledge.org>
2012-08-16 19:36:30 +0200

    spell out constraint names, create necessary indices

--- ledge-components/src/main/sql/naming/DBNamingTables.sql
+++ ledge-components/src/main/sql/naming/DBNamingTables.sql
@@ -6,11 +6,7 @@ CREATE TABLE ledge_naming_context
     PRIMARY KEY (context_id)
 );
 
--- to powinno robic sie samo
-CREATE UNIQUE INDEX ledge_naming_context_pk ON ledge_naming_context (context_id);
-CREATE UNIQUE INDEX ledge_naming_context_dn ON ledge_naming_context (dn);
-
--- #####################################################
+CREATE UNIQUE INDEX ledge_naming_context_dn_idx ON ledge_naming_context (dn);
 
 CREATE TABLE ledge_naming_attribute
 (
@@ -19,11 +15,7 @@ CREATE TABLE ledge_naming_attribute
     value VARCHAR(64) NOT NULL,
 
     PRIMARY KEY (context_id, name, value),
-    FOREIGN KEY (context_id) REFERENCES ledge_naming_context
+    CONSTRAINT ledge_naming_attribute_context_id_fkey FOREIGN KEY (context_id) REFERENCES ledge_naming_context(context_id)
 );
 
--- to powinno samo sie zrobic
-CREATE INDEX ledge_naming_attribute_pk ON ledge_naming_attribute (context_id, name, value);
-
--- a to nie
-CREATE INDEX ledge_naming_context_id_fk ON ledge_naming_attribute (context_id);
+CREATE INDEX ledge_naming_context_id_idx ON ledge_naming_attribute (context_id);
