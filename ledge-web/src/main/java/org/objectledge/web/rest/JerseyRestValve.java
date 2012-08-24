8d950aac9c182f1d60b68fec19c4ef96eaa12941
Rafał Krzewski <rafal.krzewski@objectledge.org>
2012-08-24 13:47:09 +0200

    Refactored to pull in a ServletContext object through the constructor and generate a sythetic ServletContext object, passing settings from valve's configuration as initParameters

--- ledge-web/src/main/java/org/objectledge/web/rest/JerseyRestValve.java
+++ ledge-web/src/main/java/org/objectledge/web/rest/JerseyRestValve.java
@@ -1,6 +1,8 @@
 package org.objectledge.web.rest;
 
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Hashtable;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
@@ -17,54 +19,107 @@ import org.objectledge.pipeline.Valve;
 import org.objectledge.web.HttpContext;
 
 import com.sun.jersey.api.core.PackagesResourceConfig;
+import com.sun.jersey.spi.container.servlet.ServletContainer;
 
+public class JerseyRestValve
+    implements Valve
+{
+    private final ServletContainer jerseyContainer;
 
-public class JerseyRestValve implements RestProcessor, Valve {	
-    
-    private volatile LedgeServletContainer jerseyContainer;	
-    
-    Logger logger;
+    private final Logger logger;
 
-	private String restPackageNames;
-
-    
     /**
      * Creates a new jersey REST dispatcher.
      * 
      * @param pipeline the pipeline
      * @param context the thread context
      * @throws ConfigurationException if the configuration is malformed.
-     * @throws ServletException 
+     * @throws ServletException
      */
-	public JerseyRestValve(Logger logger, Configuration config) throws ConfigurationException, ServletException {
-       this.logger = logger;
-       restPackageNames = config.getChild("restPackageNames").getValue();  
-	}
+    public JerseyRestValve(Logger logger, final Configuration config,
+        final ServletContext servletContext)
+        throws ConfigurationException, ServletException
+    {
+        this.logger = logger;
+        String restPackageNames = config.getChild("restPackageNames").getValue();
 
-	@Override
-	public void process(Context context) throws ProcessingException {
-		HttpContext httpContext = (HttpContext)context.getAttribute(HttpContext.class);
-	    HttpServletRequest request = httpContext.getRequest();
- 	    HttpServletResponse response = httpContext.getResponse(); 	    
- 	    ServletConfig svconfig = (ServletConfig)context.getAttribute(ServletConfig.class);
-// 	    ServletContext svcontext = svconfig.getServletContext();
-// 	    svcontext.setAttribute("ledgeContext", context); //for Jersey rest resources to access data sources
-// 	    
-        try {
-            if(jerseyContainer == null) {
-                PackagesResourceConfig app = new PackagesResourceConfig(restPackageNames);
-                jerseyContainer = new LedgeServletContainer(app);
-                jerseyContainer.setServletConfig(svconfig);
-                jerseyContainer.init();                 
+        Configuration initParams = config.getChild("init-parmameters", true);
+
+        final ServletConfig ledgeServletConfig = new LedgeServletConfig(servletContext, initParams);
+        jerseyContainer = new ServletContainer(new PackagesResourceConfig(restPackageNames))
+            {
+                @Override
+                public ServletConfig getServletConfig()
+                {
+                    return ledgeServletConfig;
+                }
+            };
+        jerseyContainer.init();
+    }
+
+    @Override
+    public void process(Context context)
+        throws ProcessingException
+    {
+        HttpContext httpContext = context.getAttribute(HttpContext.class);
+        HttpServletRequest request = httpContext.getRequest();
+        HttpServletResponse response = httpContext.getResponse();
+        try
+        {
+            jerseyContainer.service(request, response);
+            httpContext.setDirectResponse(true);
+        }
+        catch(ServletException e)
+        {
+            throw new ProcessingException(e);
+        }
+        catch(IOException e)
+        {
+            throw new ProcessingException(e);
+        }
+    }
+
+    private static class LedgeServletConfig
+        implements ServletConfig
+    {
+        private final ServletContext context;
+
+        private Hashtable<String, String> parameters = new Hashtable<String, String>();
+
+        public LedgeServletConfig(ServletContext context, Configuration config)
+            throws ConfigurationException
+        {
+            this.context = context;
+            for(Configuration param : config.getChildren("init-param"))
+            {
+                final String name = param.getChild("param-name").getValue();
+                final String value = param.getChild("param-value").getValue();
+                parameters.put(name, value);
             }
-        	jerseyContainer.service(request, response);
-        	httpContext.setDirectResponse(true);
-        	
-		} catch (ServletException e) {
-			throw new ProcessingException(e);
-		} catch (IOException e) {
-			throw new ProcessingException(e);
-		}
-	}
+        }
 
+        @Override
+        public String getServletName()
+        {
+            return "REST";
+        }
+
+        @Override
+        public ServletContext getServletContext()
+        {
+            return context;
+        }
+
+        @Override
+        public String getInitParameter(String name)
+        {
+            return parameters.get(name);
+        }
+
+        @Override
+        public Enumeration getInitParameterNames()
+        {
+            return parameters.keys();
+        }
+    }
 }
