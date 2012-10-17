bdb49ee2b82f109e39b2aea7b3043138f2138550
Lukasz Urbanski <lukasz@caltha.pl>
2012-10-17 16:28:20 +0200

    BAZYNGO-269: JerseyRestValve configuration fix.

--- ledge-web/src/main/java/org/objectledge/web/rest/JerseyRestValve.java
+++ ledge-web/src/main/java/org/objectledge/web/rest/JerseyRestValve.java
@@ -3,6 +3,7 @@ package org.objectledge.web.rest;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.Map;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
@@ -47,8 +48,10 @@ public class JerseyRestValve
 
         Configuration initParams = config.getChild("init-parameters", true);
 
-        final ServletConfig ledgeServletConfig = new LedgeServletConfig(servletContext, initParams);
-        jerseyContainer = new ServletContainer(new PackagesResourceConfig(restPackageNames))
+        final LedgeServletConfig ledgeServletConfig = new LedgeServletConfig(servletContext, initParams);
+        PackagesResourceConfig packagesResourceConfig = new PackagesResourceConfig(restPackageNames);
+        packagesResourceConfig.setPropertiesAndFeatures(ledgeServletConfig.getParameters());
+        jerseyContainer = new ServletContainer(packagesResourceConfig)
             {
                 @Override
                 public ServletConfig getServletConfig()
@@ -86,7 +89,7 @@ public class JerseyRestValve
     {
         private final ServletContext context;
 
-        private Hashtable<String, String> parameters = new Hashtable<String, String>();
+        private Hashtable<String, Object> parameters = new Hashtable<String, Object>();
 
         public LedgeServletConfig(ServletContext context, Configuration config)
             throws ConfigurationException
@@ -115,7 +118,7 @@ public class JerseyRestValve
         @Override
         public String getInitParameter(String name)
         {
-            return parameters.get(name);
+            return (String)parameters.get(name);
         }
 
         @Override
@@ -123,5 +126,11 @@ public class JerseyRestValve
         {
             return parameters.keys();
         }
+        
+        public Map<String, Object> getParameters()
+        {
+            return (Map<String, Object>)parameters;
+        }
+        
     }
 }
