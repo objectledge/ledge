0f60a8cf53c047d095840f224268459b9c80ebe8
Rafał Krzewski <rafal.krzewski@objectledge.org>
2012-12-01 14:09:48 +0100

    LEDGE-208: Added support for filtering operation list by user or operation code

--- ledge-web/src/main/java/org/objectledge/modules/views/longops/ActiveOperations.java
+++ ledge-web/src/main/java/org/objectledge/modules/views/longops/ActiveOperations.java
@@ -1,10 +1,22 @@
 package org.objectledge.modules.views.longops;
 
+import java.io.IOException;
+import java.security.Principal;
+import java.util.Collection;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.codehaus.jackson.JsonNode;
 import org.jcontainer.dna.Logger;
+import org.objectledge.authentication.AuthenticationException;
+import org.objectledge.authentication.UserManager;
 import org.objectledge.context.Context;
+import org.objectledge.longops.LongRunningOperation;
 import org.objectledge.longops.LongRunningOperationRegistry;
+import org.objectledge.parameters.Parameters;
+import org.objectledge.parameters.RequestParameters;
 import org.objectledge.pipeline.ProcessingException;
+import org.objectledge.web.HttpContext;
 import org.objectledge.web.json.AbstractJsonView;
 
 public class ActiveOperations
@@ -12,16 +24,54 @@ public class ActiveOperations
 {
     private final LongRunningOperationRegistry registry;
 
-    public ActiveOperations(LongRunningOperationRegistry registry, Context context, Logger log)
+    private final UserManager userManager;
+
+    public ActiveOperations(LongRunningOperationRegistry registry, UserManager userManager,
+        Context context, Logger log)
     {
         super(context, log);
         this.registry = registry;
+        this.userManager = userManager;
     }
 
     @Override
     protected JsonNode buildJsonTree()
         throws ProcessingException
     {
-        return objectMapper.valueToTree(registry.getActiveOperations());
+        Parameters parameters = context.getAttribute(RequestParameters.class);
+        String uid = parameters.get("uid", null);
+        String code = parameters.get("code", null);
+        Collection<LongRunningOperation> activeOperations;
+        try
+        {
+            if(uid != null)
+            {
+                Principal user = userManager.getUserByLogin(uid);
+                activeOperations = registry.getActiveOperations(user);
+            }
+            else if(code != null)
+            {
+                activeOperations = registry.getActiveOperations(code);
+            }
+            else
+            {
+                activeOperations = registry.getActiveOperations();
+            }
+            return objectMapper.valueToTree(activeOperations);
+        }
+        catch(IllegalArgumentException | AuthenticationException e)
+        {
+            HttpContext httpContext = context.getAttribute(HttpContext.class);
+            try
+            {
+                httpContext.getResponse().sendError(HttpServletResponse.SC_BAD_REQUEST,
+                    e.getMessage());
+            }
+            catch(IOException e1)
+            {
+                log.error("failed to send error response to client", e);
+            }
+            throw new ProcessingException(e);
+        }
     }
 }
