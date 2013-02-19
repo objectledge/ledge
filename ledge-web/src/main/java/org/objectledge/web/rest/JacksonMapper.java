58c124c3d674205417c16561bef163cc55d9dac6
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-02-19 14:48:53 +0100

    LEDGE-223 Add contract which must be implemented to register as configuration provider for some kind of POJO

---
+++ ledge-web/src/main/java/org/objectledge/web/rest/JacksonMapper.java
@@ -0,0 +1,29 @@
+package org.objectledge.web.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * JacksonMapper is a specific configuration for mapping your objects if default is not what your
+ * into.
+ * 
+ * @author Marek Lewandowski
+ * @since 2013-02-19
+ */
+public interface JacksonMapper
+{
+    /**
+     * Returns true if this mapper has configuration for this type
+     * 
+     * @param type - type of object for which configuration is sought
+     * @return <code>true</code> if this mapper has configuration for this type, <code>false</code>
+     *         otherwise
+     */
+    boolean providesFor(Class<?> type);
+
+    /**
+     * Returns configured ObjectMapper
+     * 
+     * @return objectMapper the ObjectMapper with specific configuration for asked type
+     */
+    ObjectMapper getMapper();
+}
