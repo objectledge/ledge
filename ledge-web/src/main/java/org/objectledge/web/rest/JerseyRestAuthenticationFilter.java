77b06d1d07fc18ca265c6b7661cb1f67508a6c4a
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-02-21 15:10:36 +0100

    BAZYNGO-654 Add annotation so this filter will trigger on annotated methods of resources. Make it work with @Inject and clean up after changes to ServerApiRestrictionProvider.

--- ledge-web/src/main/java/org/objectledge/web/rest/JerseyRestAuthenticationFilter.java
+++ ledge-web/src/main/java/org/objectledge/web/rest/JerseyRestAuthenticationFilter.java
@@ -2,6 +2,7 @@ package org.objectledge.web.rest;
 
 import java.io.IOException;
 
+import javax.inject.Inject;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
@@ -13,41 +14,26 @@ import org.objectledge.authentication.api.ServerApiRestrictions.AutorizationStat
 import org.objectledge.context.Context;
 import org.objectledge.web.HttpContext;
 
-
+@ApiRestrictions
 public class JerseyRestAuthenticationFilter
     implements ContainerRequestFilter
 {
-    private final ServerApiRestrictionProvider[] providers;
-
-    private final Context context;
-
-    public JerseyRestAuthenticationFilter(ServerApiRestrictionProvider[] providers,
-        Context context)
-    {
-        this.providers = providers;
-        this.context = context;
-    }
+    @Inject
+    private ServerApiRestrictionProvider provider;
 
     @Override
     public void filter(ContainerRequestContext requestContext)
         throws IOException
     {
         AutorizationStatus response = AutorizationStatus.UNDEFINED;
-        HttpContext httpContext = context.getAttribute(HttpContext.class);
-        for(ServerApiRestrictionProvider provider : providers)
-        {
-            ServerApiRestrictions serverApiRestrictions = provider.getServerApiRestrictions();
-            if(serverApiRestrictions == null)
-                continue;
 
-            response = serverApiRestrictions.validateApiRequest(requestContext.getUriInfo()
-                .getPath(), requestContext.getMethod(), requestContext
-                .getHeaderString("Authorization"), httpContext.getRequest().getRemoteAddr(),
-                requestContext.getSecurityContext().isSecure());
+        HttpContext httpContext = new Context().getAttribute(HttpContext.class);
+        ServerApiRestrictions serverApiRestrictions = provider.getServerApiRestrictions();
+        response = serverApiRestrictions.validateApiRequest(requestContext.getUriInfo().getPath(),
+            requestContext.getMethod(), requestContext.getHeaderString("Authorization"),
+            httpContext.getRequest().getRemoteAddr(), requestContext.getSecurityContext()
+                .isSecure());
 
-            if(!AutorizationStatus.UNDEFINED.equals(response))
-                break;
-        }
         if(AutorizationStatus.UNAUTHORIZED.equals(response))
         {
             throw new WebApplicationException(Response.Status.UNAUTHORIZED);
