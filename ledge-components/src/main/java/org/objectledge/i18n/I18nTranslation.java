079908a5d9f70610a9eb440055cfc68ea5eb46be
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-03-27 13:20:20 +0100

    BAZYNGO-843 Add I18N translations

---
+++ ledge-components/src/main/java/org/objectledge/i18n/I18nTranslation.java
@@ -0,0 +1,27 @@
+package org.objectledge.i18n;
+
+import java.util.Locale;
+
+public final class I18nTranslation
+{
+    private final Locale locale;
+
+    private final String translation;
+
+    public I18nTranslation(Locale locale, String translation)
+    {
+        this.locale = locale;
+        this.translation = translation;
+    }
+
+    public Locale getLocale()
+    {
+        return locale;
+    }
+
+    public String getTranslation()
+    {
+        return translation;
+    }
+
+}
