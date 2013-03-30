cec51c04c9f6afe03acf53b697fa6949cc9beba2
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-03-30 11:42:09 +0100

    BAZYNGO-878 Add Factory which will create instances of type I18nLocale

---
+++ ledge-web/src/main/java/org/objectledge/web/rest/I18nLocaleFactory.java
@@ -0,0 +1,25 @@
+package org.objectledge.web.rest;
+
+import org.glassfish.hk2.api.Factory;
+import org.objectledge.context.Context;
+import org.objectledge.i18n.I18nContext;
+import org.objectledge.i18n.I18nLocale;
+
+public class I18nLocaleFactory
+    implements Factory<I18nLocale>
+{
+
+    @Override
+    public void dispose(I18nLocale instance)
+    {
+
+    }
+
+    @Override
+    public I18nLocale provide()
+    {
+        final I18nContext i18nContext = I18nContext.getI18nContext(new Context());
+        return i18nContext;
+    }
+
+}
