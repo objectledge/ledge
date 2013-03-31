2942d42309dfd450f471c0d89bcadd5781f0fd5a
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2013-03-31 23:26:04 +0200

    BAZYNGO-896 Add some handy method to read json as collection of some type

--- ledge-web/src/main/java/org/objectledge/web/json/ObjectMapperProvider.java
+++ ledge-web/src/main/java/org/objectledge/web/json/ObjectMapperProvider.java
@@ -1,8 +1,14 @@
 package org.objectledge.web.json;
 
+import java.io.IOException;
+import java.util.Collection;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public interface ObjectMapperProvider
 {
     ObjectMapper provide();
+
+    <T> Collection<T> readAsCollectionOf(String json, Class<T> clazz)
+        throws IOException;
 }
--- ledge-web/src/main/java/org/objectledge/web/json/ObjectMapperProviderImpl.java
+++ ledge-web/src/main/java/org/objectledge/web/json/ObjectMapperProviderImpl.java
@@ -1,6 +1,11 @@
 package org.objectledge.web.json;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 
 public class ObjectMapperProviderImpl
     implements ObjectMapperProvider
@@ -12,6 +17,15 @@ public class ObjectMapperProviderImpl
     public ObjectMapper provide()
     {
         return mapper;
+    }
+
+    @Override
+    public <T> Collection<T> readAsCollectionOf(String json, Class<T> clazz)
+        throws IOException
+    {
+        final CollectionType collectionType = mapper.getTypeFactory().constructCollectionType(
+            ArrayList.class, clazz);
+        return mapper.readValue(json, collectionType);
     }
 
 }
