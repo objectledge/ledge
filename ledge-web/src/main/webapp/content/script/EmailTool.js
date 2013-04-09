ade11b06a93c2b36bbaf1736ff0813df59ae2c56
Lukasz Urbanski <lukasz@caltha.pl>
2013-04-09 14:10:48 +0200

    BAZYNGO-928: "js fix."

--- ledge-web/src/main/webapp/content/script/EmailTool.js
+++ ledge-web/src/main/webapp/content/script/EmailTool.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
 
-	jQuery("span.emailEncode").each(function(i, v) {
+	jQuery("span.emailEncode").each(function() {
 		var data_encoded = $(this).attr("data-encoded");
 		if (data_encoded) {
 			$(this).text(unescape(data_encoded));
