1fb2cc50715e9759a9ee73a05d266405ce1c1e5a
Rafał Krzewski <rafal.krzewski@objectledge.org>
2014-01-31 13:59:43 +0100

    BAZYNGO-1457: added reflection based property accessor

---
+++ ledge-web/src/main/java/org/objectledge/table/comparator/ReflectionPropertyAccessor.java
@@ -0,0 +1,43 @@
+package org.objectledge.table.comparator;
+
+import java.lang.reflect.Field;
+
+public class ReflectionPropertyAccessor<O, V>
+    implements PropertyBasedComparator.PropertyAccessor<O, V>
+{
+    private Field field;
+
+    private final Class<V> fieldClass;
+
+    public ReflectionPropertyAccessor(Class<O> objectClass, String fieldName, Class<V> fieldClass)
+        throws NoSuchFieldException, SecurityException
+    {
+        this.fieldClass = fieldClass;
+        this.field = objectClass.getDeclaredField(fieldName);
+        if(!field.getClass().isAssignableFrom(fieldClass))
+        {
+            throw new IllegalArgumentException(fieldName + " has type " + field.getClass()
+                + " not compatible with " + fieldClass);
+        }
+        this.field.setAccessible(true);
+    }
+
+    @Override
+    public Class<V> getType()
+    {
+        return fieldClass;
+    }
+
+    @Override
+    public V getValue(O o)
+    {
+        try
+        {
+            return fieldClass.cast(field.get(o));
+        }
+        catch(IllegalAccessException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+}
