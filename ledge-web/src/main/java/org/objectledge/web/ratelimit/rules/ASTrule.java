a9eceb9faff01cc1fa127e3a1707cf99814898b7
Rafał Krzewski <rafal.krzewski@caltha.pl>
2014-09-08 09:46:35 +0200

    LEDGE-255: save action token in AST

--- ledge-web/src/main/java/org/objectledge/web/ratelimit/rules/ASTrule.java
+++ ledge-web/src/main/java/org/objectledge/web/ratelimit/rules/ASTrule.java
@@ -2,20 +2,30 @@
 /* JavaCCOptions:MULTI=true,NODE_USES_PARSER=false,VISITOR=true,TRACK_TOKENS=false,NODE_PREFIX=AST,NODE_EXTENDS=,NODE_FACTORY=,SUPPORT_CLASS_VISIBILITY_PUBLIC=true */
 package org.objectledge.web.ratelimit.rules;
 
-public
-class ASTrule extends SimpleNode {
-  public ASTrule(int id) {
-    super(id);
-  }
+public class ASTrule
+    extends SimpleNode
+{
+    private String action;
 
-  public ASTrule(RateLimitRules p, int id) {
-    super(p, id);
-  }
+    public ASTrule(int id)
+    {
+        super(id);
+    }
 
+    public void setAction(Token token)
+    {
+        this.action = token.image;
+    }
+    
+    public String getAction()
+    {
+        return action;
+    }
 
-  /** Accept the visitor. **/
-  public Object jjtAccept(RateLimitRulesVisitor visitor, Object data) {
-    return visitor.visit(this, data);
-  }
+    /** Accept the visitor. **/
+    public Object jjtAccept(RateLimitRulesVisitor visitor, Object data)
+    {
+        return visitor.visit(this, data);
+    }
 }
 /* JavaCC - OriginalChecksum=e5bbf06d7caa61035a3028644f4bf9e3 (do not edit this line) */
