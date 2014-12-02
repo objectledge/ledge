814e04c43d9cfd46df20ad249cd05fe9403b7431
Rafał Krzewski <rafal.krzewski@caltha.pl>
2014-12-02 18:44:23 +0100

    LEDGE-255: a factory method creating RequestInfo of a HttpServletRequest

--- ledge-web/src/main/java/org/objectledge/web/ratelimit/impl/RequestInfo.java
+++ ledge-web/src/main/java/org/objectledge/web/ratelimit/impl/RequestInfo.java
@@ -1,9 +1,15 @@
 package org.objectledge.web.ratelimit.impl;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Collections;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.objectledge.net.IPAddressUtil;
 
 public class RequestInfo
 {
@@ -41,5 +47,34 @@ public class RequestInfo
     public String getPath()
     {
         return path;
+    }
+
+    public static RequestInfo of(HttpServletRequest req)
+    {
+        try
+        {
+            Map<String, String> headers = new HashMap<>();
+            @SuppressWarnings("unchecked")
+            Enumeration<String> he = req.getHeaderNames();
+            while(he.hasMoreElements())
+            {
+                String header = he.nextElement();
+                headers.put(header, req.getHeader(header));
+            }
+            StringBuilder b = new StringBuilder();
+            b.append(req.getContextPath());
+            b.append(req.getServletPath());
+            if(req.getPathInfo() != null)
+            {
+                b.append(req.getPathInfo());
+            }
+            return new RequestInfo(IPAddressUtil.byAddress(req.getRemoteAddr()),
+                req.getRemoteHost(), headers, b.toString());
+        }
+        catch(UnknownHostException | IllegalArgumentException e)
+        {
+            throw new IllegalArgumentException(
+                "Invalid address retured by HttpServletRequest.getRemoteAddr()", e);
+        }
     }
 }
